2e3d8483863494e5d9ad92937572ce421efcd275
kitaj <kitaj@7f22e88f-374d-0410-998f-c91420d97ba2>
2002-03-11 03:17:19 +0000

    tabify

--- plugin/calendar2.rb
+++ plugin/calendar2.rb
@@ -1,4 +1,4 @@
-# calendar2.rb $Revision: 1.3 $
+# calendar2.rb $Revision: 1.4 $
 #
 # calendar2: どこかで見たようなカレンダーを日記に追加する
 #   パラメタ:
@@ -11,50 +11,50 @@
 # Distributed under the GPL
 #
 def calendar2_make_cal(year, month)
-  result = []
-  t = Time.local(year, month, 1)
-  r = Array.new(t.wday, nil)
-  r << 1
-  2.upto(31) do |i|
-    break if Time.local(year, month, i).month != month
-    r << i
-  end
-  r += Array.new((- r.size) % 7, nil)
-   0.step(r.size - 1, 7) do |i|
-     result << r[i, 7]
-  end
-  result
+	result = []
+	t = Time.local(year, month, 1)
+	r = Array.new(t.wday, nil)
+	r << 1
+	2.upto(31) do |i|
+		break if Time.local(year, month, i).month != month
+		r << i
+	end
+	r += Array.new((- r.size) % 7, nil)
+	0.step(r.size - 1, 7) do |i|
+		result << r[i, 7]
+	end
+	result
 end
 
 def calendar2_prev_current_next
-  yyyymm = @date.strftime "%Y%m"
-  yms = [yyyymm]
-  @years.keys.each do |y|
-    yms |= @years[y].collect {|i| y + i}
-  end
-  yms.sort!
-  yms.unshift nil
-  yms.push nil
-  i = yms.index(yyyymm)
-  yms[i - 1, 3]
+	yyyymm = @date.strftime "%Y%m"
+	yms = [yyyymm]
+	@years.keys.each do |y|
+		yms |= @years[y].collect {|i| y + i}
+	end
+	yms.sort!
+	yms.unshift nil
+	yms.push nil
+	i = yms.index(yyyymm)
+	yms[i - 1, 3]
 end
 
 def calendar2_make_anchor(ym, str)
-  if ym
-    %Q|<a href="#{@index}#{anchor ym}">#{str}</a>|
-  else
-    str
-  end
+	if ym
+		%Q|<a href="#{@index}#{anchor ym}">#{str}</a>|
+	else
+		str
+	end
 end
 
 def calendar2(days_format = "日月火水木金土".split(//),
-              nav_format = ["前", "%d年<br>%d月", "次"])
-  return '' if /TAMATEBAKO/ =~ ENV["HTTP_USER_AGENT"]
-  year = @date.year
-  month = @date.month  
-  p_c_n = calendar2_prev_current_next
+					nav_format = ["前", "%d年<br>%d月", "次"])
+	return '' if /TAMATEBAKO/ =~ ENV["HTTP_USER_AGENT"]
+	year = @date.year
+	month = @date.month  
+	p_c_n = calendar2_prev_current_next
 
-  result = <<CALENDAR_HEAD
+	result = <<CALENDAR_HEAD
 <table class="calendar" summary="calendar">
 <tr>
  <td class="calendar-prev-month" colspan="2">#{calendar2_make_anchor(p_c_n[0], nav_format[0] % [year, month])}</td>
@@ -62,41 +62,40 @@ def calendar2(days_format = "日月火水木金土".split(//),
  <td class="calendar-next-month" colspan="2">#{calendar2_make_anchor(p_c_n[2], nav_format[2] % [year, month])}</td>
 </tr>
 CALENDAR_HEAD
-  result << "<tr>"
-  result << %Q| <td class="calendar-sunday">#{days_format[0]}</td>\n|
-  1.upto(5) do |i|
-    result << %Q| <td class="calendar-weekday">#{days_format[i]}</td>\n|
-  end
-  result << %Q| <td class="calendar-saturday">#{days_format[6]}</td>\n|
-  result << "</tr>\n"
-  calendar2_make_cal(year, month).each do |week|
-    result << "<tr>\n"
-    week.each do |day|
-      if day == nil
-        result << %Q| <td class="calendar-day"></td>\n|
-      else
-        date = "%04d%02d%02d" % [year, month, day]
-        result << %Q| <td class="calendar-day">%s</td>\n| %
-          if @diaries[date] == nil or ! @diaries[date].visible?
-            day.to_s
-          else
-            subtitles = []
-            idx = "01"
-            @diaries[date].each_paragraph do |paragraph|
-              if paragraph.subtitle
-                subtitles << %Q|#{idx}. #{CGI::escapeHTML(paragraph.subtitle.gsub(/<.+?>/, ''))}|
-                idx.succ!
-              end
-            end
-            %Q|<a href="#{@index}#{anchor date}" title="#{subtitles.join("&#13;&#10;")}">#{day}</a>|
-          end
-      end
-    end
-    result << "</tr>\n"
-  end
-  result << "</table>\n"
+	result << "<tr>"
+	result << %Q| <td class="calendar-sunday">#{days_format[0]}</td>\n|
+	1.upto(5) do |i|
+		result << %Q| <td class="calendar-weekday">#{days_format[i]}</td>\n|
+	end
+	result << %Q| <td class="calendar-saturday">#{days_format[6]}</td>\n|
+	result << "</tr>\n"
+	calendar2_make_cal(year, month).each do |week|
+		result << "<tr>\n"
+		week.each do |day|
+			if day == nil
+				result << %Q| <td class="calendar-day"></td>\n|
+			else
+				date = "%04d%02d%02d" % [year, month, day]
+				result << %Q| <td class="calendar-day">%s</td>\n| %
+					if @diaries[date] == nil or ! @diaries[date].visible?
+						day.to_s
+					else
+						subtitles = []
+						idx = "01"
+						@diaries[date].each_paragraph do |paragraph|
+							if paragraph.subtitle
+								subtitles << %Q|#{idx}. #{CGI::escapeHTML(paragraph.subtitle.gsub(/<.+?>/, ''))}|
+								idx.succ!
+							end
+						end
+						%Q|<a href="#{@index}#{anchor date}" title="#{subtitles.join("&#13;&#10;")}">#{day}</a>|
+					end
+			end
+		end
+		result << "</tr>\n"
+	end
+	result << "</table>\n"
 end
 
 #@calendar2_cache = CacheMonth.new(@date, :calender2, method(:calendar2))
 #add_update_proc @calendar2_cache.writer
-
--- plugin/recent_list.rb
+++ plugin/recent_list.rb
@@ -1,4 +1,4 @@
-# $Revision: 1.3 $
+# $Revision: 1.4 $
 # recent_list: 最近書いた日記のタイトル，サブタイトルを表示する
 #   パラメタ(カッコ内は未指定時の値):
 #     days:            何日分の日記を表示するか(20)
@@ -17,62 +17,62 @@
 #
 eval( <<MODIFY_CLASS, TOPLEVEL_BINDING )
 class TDiaryMonth
-  attr_reader :diaries
+	attr_reader :diaries
 end
 
 class Paragraph
-  def shorten(len = 120)
-    lines = NKF::nkf("-e -m0 -f#{len}", @body.gsub(/<.+?>/, '')).split("\n")
-    lines[0].concat('...') if lines[0] and lines[1]
-    lines[0]
-  end
+	def shorten(len = 120)
+		lines = NKF::nkf("-e -m0 -f#{len}", @body.gsub(/<.+?>/, '')).split("\n")
+		lines[0].concat('...') if lines[0] and lines[1]
+		lines[0]
+	end
 end
 MODIFY_CLASS
 
 def recent_list(days = 30, date_format = @date_format,
-                title_with_body = false, show_size = false, show_title = false)
-  result = ""
-  cgi = CGI::new
+					title_with_body = false, show_size = false, show_title = false)
+	result = ""
+	cgi = CGI::new
 
-  catch(:exit) {
-    @years.keys.sort.reverse_each do |year|
-      @years[year].sort.reverse_each do |month|
-        cgi.params['date'] = ["#{year}#{month}"]
-        m = TDiaryMonth::new(cgi, '')
-        m.diaries.keys.sort.reverse_each do |date|
-          next unless m.diaries[date].visible?
-          result << %Q|<p class="recentitem"><a href="#{@index}?date=#{date}">#{m.diaries[date].date.strftime(date_format)}</a>\n|
-          if show_title and m.diaries[date].title
-            result << %Q| #{m.diaries[date].title}|
-          end
-          if show_size == true
-            s = 0
-            m.diaries[date].each_paragraph do |paragraph|
-              s = s + paragraph.to_s.size.to_i
-            end
-            result << ":#{s}"
-          end
-          result << %Q|</p>\n<div class="recentsubtitles">\n|
+	catch(:exit) {
+		@years.keys.sort.reverse_each do |year|
+			@years[year].sort.reverse_each do |month|
+				cgi.params['date'] = ["#{year}#{month}"]
+				m = TDiaryMonth::new(cgi, '')
+				m.diaries.keys.sort.reverse_each do |date|
+					next unless m.diaries[date].visible?
+					result << %Q|<p class="recentitem"><a href="#{@index}?date=#{date}">#{m.diaries[date].date.strftime(date_format)}</a>\n|
+					if show_title and m.diaries[date].title
+						result << %Q| #{m.diaries[date].title}|
+					end
+					if show_size == true
+						s = 0
+						m.diaries[date].each_paragraph do |paragraph|
+							s = s + paragraph.to_s.size.to_i
+						end
+						result << ":#{s}"
+					end
+					result << %Q|</p>\n<div class="recentsubtitles">\n|
 
-          i = 1
-          m.diaries[date].each_paragraph do |paragraph|
-            if paragraph.subtitle
-              result << %Q| <a href="#{@index}?date=#{date}#p#{'%02d' % i}"|
-              result << %Q| title="#{CGI::escapeHTML(paragraph.shorten)}"| \
-                if title_with_body == true
-              result << %Q|>#{i}</a>. | \
-                     << %Q|#{paragraph.subtitle}<br>\n|
-            end
-            i += 1
-          end
-          result << "</div>\n"
-          days -= 1
-          throw :exit if days == 0
-        end
-      end
-    end
-  }
-  result
+					i = 1
+					m.diaries[date].each_paragraph do |paragraph|
+						if paragraph.subtitle
+							result << %Q| <a href="#{@index}?date=#{date}#p#{'%02d' % i}"|
+							result << %Q| title="#{CGI::escapeHTML(paragraph.shorten)}"| \
+								if title_with_body == true
+							result << %Q|>#{i}</a>. | \
+									<< %Q|#{paragraph.subtitle}<br>\n|
+						end
+						i += 1
+					end
+					result << "</div>\n"
+					days -= 1
+					throw :exit if days == 0
+				end
+			end
+		end
+	}
+	result
 end
 
 #@recent_list_cache = Cache.new(:recent_list, method(:recent_list), 10, '%Y/%m/%d', true, true)
