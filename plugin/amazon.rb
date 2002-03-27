f5e9cd02de5c24eeef49662ef70bd2c653f56f0b
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2002-03-27 14:49:43 +0000

    add amazon.

---
+++ plugin/amazon.rb
@@ -0,0 +1,173 @@
+# amazon.rb $Revision: 1.1 $
+#
+# isbn_image_left: 指定したISBNの書影をclass="left"で表示
+#   パラメタ:
+#     asin:    ASINまたはISBN
+#     comment: コメント
+#
+# isbn_image_right: 指定したISBNの書影をclass="right"で表示
+#   パラメタ:
+#     asin:    ASINまたはISBN
+#     comment: コメント
+#
+# isbn_image: 指定したISBNの書影をclass="amazon"で表示
+#     asin:    ASINまたはISBN
+#     comment: コメント
+#
+# isbn: amazonにアクセスしない簡易バージョン。
+#     asin:    ASINまたはISBN
+#     comment: コメント
+#
+#   ASINとはアマゾン独自の商品管理IDです。
+#   書籍のISBNをASINに入力すると書籍が表示されます。
+#
+#   それぞれ商品画像が見つからなかった場合は
+#       <a href="amazonのページ">商品名</a>
+#   のように商品名を表示します。
+#   コメントが記述されている場合は商品名がコメントの内容に変わります。
+#
+#   tdiary.confにて、@options['amazon.aid']にアソシエイトIDを指定
+#   することで、自分のアソシエイトプログラムを利用できます。
+#
+# 注意：著作権が関連する為、www.amazon.co.jpのアソシエイトプログラムを
+# 確認の上利用して下さい。
+#
+# Copyright (C) 2002 by HAL99 <hal99@mtj.biglobe.ne.jp>
+#
+# Original: HAL99 <hal99@mtj.biglobe.ne.jp>
+# Modified: by TADA Tadashi<sho@spc.gr.jp>,
+#              kazuhiki<kazuhi@ring.gr.jp>,
+#              woods<sodium@da3.so-net.ne.jp>,
+#              munemasa<munemasa@t3.rim.or.jp>,
+#              dai<dai@kato-agri.com>
+#
+
+require 'net/http'
+require 'timeout'
+
+private :getAmazon
+def getAmazon(asin)
+
+  cache = "#{@cache_path}/amazon"
+
+  Dir::mkdir(cache) unless File::directory?(cache)
+  begin
+    item = File::readlines( "#{cache}/#{asin}")
+    raise if item.length < 2
+
+    return item
+  rescue
+  end
+
+  limittime = 10
+
+  host = 'www.amazon.co.jp'
+  path = "/exec/obidos/ASIN/#{asin}"
+  port = '80'
+
+  item_url = nil
+  item_name = nil
+  img_url = nil
+  img_name = nil
+  img_height = nil
+  img_width = nil
+
+  timeout (limittime) do
+    begin
+      Net::HTTP.start( host, port ) do |http|
+
+	response , = http.get(path)
+	response.body.each do |line|
+	  line = NKF::nkf("-e",line)
+	  if line =~ /^.*?:.*?:(.*?)<\/title>/i
+	    item_name = CGI::escapeHTML(CGI::unescapeHTML($1))
+	  end
+	  if line =~ /(<img src="(http\:\/\/images\.amazon\.com\/images\/P\/(.*MZZZZZZZ.jpg))".*?>)/i
+	    img_tag = $1
+	    img_url = $2
+	    img_name = $3
+	    if img_tag =~ / width="?(\d+)"?/i
+	      img_width = $1
+	    end
+	    if img_tag =~ / height="?(\d+)"?/i
+	      img_height = $1
+	    end
+
+	  end
+	end
+      end
+    rescue Net::ProtoRetriableError => err
+      item_url = err.response['location']
+      if %r|http://([^:/]*):?(\d*)(/.*)| =~ item_url then
+	host = $1
+	port = $2.to_i
+	path = $3
+	raise 'not amazon domain' if host !~ /\.amazon\.(com|co\.uk|co\.jp|de|fr)$/         
+	raise 'bad location was returned.' unless host and path
+	port = 80 if port == 0
+	retry
+      end
+    rescue
+      raise 'getting item was failed'
+    end
+  end
+  item = [item_url.strip,item_name,img_url,img_name,img_width,img_height]
+  open("#{cache}/#{asin}","w") do |f|
+    item.each do |i|
+      next unless i
+      f.print i,"\n"
+      end
+  end
+  return item
+end
+
+def amazonNoImg(item_url,item_name)
+  %Q[<a href="#{item_url.strip}">#{item_name.strip}</a>]
+end
+
+private :getAmazonImg
+def getAmazonImg(position,asin,comment)
+  begin
+
+    item = getAmazon(asin)
+    item[0].sub!( %r|[^/]+$|, @options['amazon.aid'] ) if @options['amazon.aid']
+
+    item_name = item[1]
+    item[1] = comment if comment
+    unless item[2]
+      return amazonNoImg(item[0],item[1])
+    end
+    r = ""
+    r << %Q[<a href="#{item[0].strip}">]
+    r << %Q[<img class="#{position}" src="#{item[2].strip}" ]
+    r << %Q[width="#{item[4].strip}" ] if item[4]
+    r << %Q[height="#{item[5].strip}" ] if item[5]
+    r << %Q[alt="#{item[1].strip}" />]
+    r << item[1].strip if position == "amazon"
+    r << %Q[</a>]
+  rescue
+    asin
+  end
+end
+
+def isbnImgLeft(asin,comment = nil)
+  getAmazonImg("left",asin,comment)
+end
+alias isbn_image_left isbnImgLeft
+
+def isbnImgRight(asin,comment = nil)
+  getAmazonImg("right",asin,comment)
+end
+alias isbn_image_right isbnImgRight
+
+def isbnImg(asin,comment = nil)
+  getAmazonImg("amazon",asin,comment)
+end
+alias isbn_image isbnImg
+alias amazon isbnImg
+
+def isbn(asin,comment)
+  item_url = "http://www.amazon.co.jp/exec/obidos/ASIN/#{asin}/"
+  item_url.sub!( %r|[^/]+$|, @options['amazon.aid'] ) if @options['amazon.aid']
+  amazonNoImg(item_url,comment)
+end
