300e5f9de6a2b58c4e336eaae41458c3320f2917
ikezoe <ikezoe@7f22e88f-374d-0410-998f-c91420d97ba2>
2002-04-24 00:02:13 +0000

    add recent_namazu

---
+++ plugin/recent_namazu.rb
@@ -0,0 +1,37 @@
+# recent_namazu.rb $Revision 1.0 $
+#
+# recent_namazu: Namazu検索語新しい順
+# 		 namazi.cgiが作成する検索キーワードログ(NMZ.slog)から
+#		 最新xx件分の検索語を表示します。
+# パラメタ:
+#   file:   検索キーワードログファイル名(絶対パス表記) 
+#   namazu: なまずcgi名 
+#   limit:  表示件数(未指定時:5) 
+#   sep:    セパレータ(未指定時:空白)
+#
+#
+# Copyright (c) 2002 Hiroyuki Ikezoe <zoe@kasumi.sakura.ne.jp>
+# Distributed under the GPL
+
+def recent_namazu(file, namazu, limit = 5, sep='&nbsp;')
+	begin
+		lines = []
+		log = open(file)
+		if log.stat.size > 300 * limit then
+			log.seek(-300 * limit,IO::SEEK_END)
+		end
+		log.each_line do |line|
+			lines << line
+		end
+
+		result = []
+		lines.reverse.each_with_index do |line,idx|
+			break if idx >= limit
+			word = line.split('\t')[0]
+			result << %Q[<a href="#{namazu}?query=#{CGI::escapeHTML(word)}">#{CGI::escapeHTML(word)}</a>]
+		end
+		result.join( sep )
+	rescue
+		%Q[<p class="message">#$! (#{$!.type})<br>cannot read #{file}.</p>]
+	end
+end
