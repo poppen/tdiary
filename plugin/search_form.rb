b3629f5f07506b69038be389c000d0de774cf6d7
mutoh <mutoh@7f22e88f-374d-0410-998f-c91420d97ba2>
2002-10-13 14:46:43 +0000

    * Remove Lycos search. * Modify Google search. * Add Image for Yahoo! search.

--- plugin/search_form.rb
+++ plugin/search_form.rb
@@ -1,11 +1,11 @@
-# search_form.rb $Revision: 1.4 $
+# search_form.rb $Revision: 1.5 $
 # -pv-
 #
 # 名称：
 # 検索フォーム表示プラグイン
 #
 # 概要：
-# Namazu, Google, Yahoo!, Lycos用の検索フォームを表示します。
+# Namazu, Google, Yahoo!用の検索フォームを表示します。
 #
 # 使う場所：
 # ヘッダ、もしくはフッタ
@@ -27,10 +27,6 @@
 # 			size:				テキストボックスの幅(省略可)
 # 			default_text:	テキストボックスの初期表示文字(省略可)
 #
-# lycosj_form(button_name, size, default_text): Lycos用
-# 			button_name:	ボタン名称(省略可)
-# 			size:				テキストボックスの幅(省略可)
-#
 # 注意：
 # 各社検索エンジンをご利用になる際は、それぞれのサイトでライセンス等を
 # 確認してください。
@@ -44,6 +40,13 @@
 # Distributed under the same license terms as tDiary.
 # 
 =begin ChangeLog
+2002-10-13 MUTOH Masao <mutoh@highway.ne.jp>
+	* Google検索で検索結果が文字化けする不具合の修正
+	* Yahoo検索で検索ボックスの左側に画像を表示するようにした
+	* Lycos検索削除（日本語の文字化けへの対応方法がわからなかったため）
+     All of them were pointed out by patagon.
+	* version 1.0.2
+
 2002-05-19 MUTOH Masao <mutoh@highway.ne.jp>
 	* ドキュメントアップデート
 	* version 1.0.1
@@ -76,17 +79,13 @@ def googlej_form(button_name = "Google 検索", size = 20, default_text = "")
 	first = %Q[<a href="http://www.google.com/">
 		<img src="http://www.google.com/logos/Logo_40wht.gif" 
 			border="0" alt="Google" align="absmiddle"></a>]
-	last = %Q[<input type=hidden name=hl value="ja">]
+	last = %Q[<input type=hidden name=hl value="ja"><input type=hidden name=ie value="euc-jp">]
 	search_form("http://www.google.com/search", "q", button_name, size, default_text, first, last)
 end
 
 def yahooj_form(button_name = "Yahoo! 検索", size = 20, default_text = "")
-	search_form("http://search.yahoo.co.jp/bin/search", "p", button_name, size, default_text)
-end
-
-def lycosj_form(button_name = " 検索 ", size = 20)
-	first = %Q[ <a href="http://www.lycos.co.jp/">
-							<img src="http://www.lycos.co.jp/images/logo_link.gif" 
-								alt="Lycos" width="70" height="20" border="0"></a>]
-	search_form("http://search.lycos.co.jp/main.html", "query", button_name, size, "", first)
+	first = %Q[<a href="http://www.google.com/">
+		<img src="http://img.yahoo.co.jp/images/yahoojp_sm.gif" 
+			border="0" alt="Yahoo! JAPAN" align="absmiddle"></a>]
+	search_form("http://search.yahoo.co.jp/bin/search", "p", button_name, size, default_text, first, "")
 end
