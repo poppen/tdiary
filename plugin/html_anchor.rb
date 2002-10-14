aca6e0b0cb644000dbfa6c30645a9561c5769e8b
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2002-10-14 09:32:47 +0000

    * remove yyyymmdd.rb. 	* add comments into html_anchor.rb.

--- plugin/html_anchor.rb
+++ plugin/html_anchor.rb
@@ -1,9 +1,16 @@
-# html_anchor $Revision: 1.1 $
+# html_anchor $Revision: 1.2 $
 #
 # anchor: アンカーを「YYYYMMDD.html」「YYYYMM.html」形式に置き換える
 #         tDiaryから自動的に呼び出されるので、プラグインファイルを
-#         設置するだけでよい。mod_rewriteと合わせて利用する。
-#         参照: http://sho.tdiary.net/20020301.html#p04
+#         設置するだけでよい。このプラグインを有効に使うためには、
+#         Webサーバ側の設定変更も必要。Apacheを使う場合、以下の2つの
+#         方法が知られている。
+#
+#         (1) mod_rewriteと合わせて利用する(推奨)
+#             参照: http://sho.tdiary.net/20020301.html#p04
+#
+#         (2) ErrorDocumentを利用する
+#             本体付属のdot.htaccessのコメントを参照
 #
 # Copyright (c) 2002 TADA Tadashi <sho@spc.gr.jp>
 # Distributed under the GPL
