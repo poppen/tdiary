692f863feaf7b2e0bc73dcdb91cabaa0d536005d
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2002-11-28 08:33:41 +0000

    * HTML 4.01 Strict support.

--- plugin/amazon.rb
+++ plugin/amazon.rb
@@ -1,4 +1,4 @@
-# amazon.rb $Revision: 1.7 $
+# amazon.rb $Revision: 1.8 $
 #
 # isbn_image_left: 指定したISBNの書影をclass="left"で表示
 #   パラメタ:
@@ -46,6 +46,9 @@
 #              dai<dai@kato-agri.com>
 #
 =begin ChangeLog
+2002-11-28 TADA Tadashi <sho@spc.gr.jp>
+	* HTML 4.01 Strict support.
+
 2002-09-01 Junichiro Kita <kita@kitaj.no-ip.com>
 	* change URL for images.
 
@@ -158,7 +161,7 @@ def getAmazonImg(position,asin,comment)
 		r << %Q[<img class="#{position}" src="#{item[2].strip}" ]
 		r << %Q[width="#{item[4].strip}" ] if item[4]
 		r << %Q[height="#{item[5].strip}" ] if item[5]
-		r << %Q[alt="#{item[1].strip}" />]
+		r << %Q[alt="#{item[1].strip}">]
 		r << item[1].strip if position == "amazon"
 		r << %Q[</a>]
 	rescue
