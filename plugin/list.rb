46609fa7965d077786b034a068f1b268c1dfba0c
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2002-12-18 01:55:26 +0000

    * remove parameter of t and s (for HTML Strict).

--- plugin/list.rb
+++ plugin/list.rb
@@ -1,43 +1,29 @@
-# list.rb $Revision: 1.1 $
+# list.rb $Revision: 1.2 $
 #
 # <ol> 順番付きリスト生成
-#   <%= ol l, t, s %>
+#   <%= ol l %>
 #   パラメタ:
 #     l: リスト文字列(\nくぎり)
-#     t: 項目番号のタイプ。
-#        1,a,A,i,I
-#        デフォルトは 1
-#     s: 開始番号
-#        デフォルトは 1
-#     (t,sは省略可能)
 #
 # <ul> 順番無しリスト
 #   <%= ul l , t %>
 #   パラメタ:
 #     l: リスト文字列(\nくぎり)
-#     t: 項目マークのタイプ。
-#          d:黒丸
-#          c:白丸
-#          s:四角
-#        デフォルトはd
-#     tは省略可能
 #
 # Copyright (c) 2002 abbey <inlet@cello.no-ip.org>
 # Distributed under the GPL.
-# 
+#
+=begin ChangeLog
+2002-12-18 TADA Tadashi <sho@spc.gr.jp>
+	* remove parameter of t and s (for HTML Strict).
+=end
 
-def ol( l, t = "1", s = "1" )
-	%Q[<ol type="#{t}" start="#{s}">#{li l}</ol>]
+def ol( l, t = nil, s = nil )
+	%Q[<ol>#{li l}</ol>]
 end
 
-def ul( l, t = "")
-	t2 = "disc"
-	if t == "c"
-		t2 = "circle"
-	elsif t == "s"
-		t2 = "square"
-	end       
-	%Q[<ul type="#{t2}">#{li l}</ul>]
+def ul( l, t = nil)
+	%Q[<ul>#{li l}</ul>]
 end
 
 def li( text )
