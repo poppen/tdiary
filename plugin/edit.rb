2332eef187a2237c961d3063a1e384f0502d6b80
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2003-02-13 05:28:28 +0000

    * edit.rb: hide preferences in categorized view.

--- plugin/edit.rb
+++ plugin/edit.rb
@@ -1,4 +1,4 @@
-# edit.rb $Revision: 1.1 $
+# edit.rb $Revision: 1.2 $
 #
 # navi_admin: 日毎表示で「更新」を「編集」に置き換える。その日の日記を
 #             すぐに編集できます。このファイルをpluginにコピーするだけ
@@ -8,8 +8,8 @@ def navi_admin
 	if @mode == 'day' then
 		result = %Q[<span class="adminmenu"><a href="#{@update}?edit=true;year=#{@date.year};month=#{@date.month};day=#{@date.day}">編集</a></span>\n]
 	else
-		result = %Q[<span class="adminmenu"><a href="#{@update}">更新</a></span>\n]
+		result = %Q[<span class="adminmenu"><a href="#{@update}">#{navi_update}</a></span>\n]
 	end
-	result << %Q[<span class="adminmenu"><a href="#{@update}?conf=OK">設定</a></span>\n] if /^(latest|month|day|comment)$/ !~ @mode
+	result << %Q[<span class="adminmenu"><a href="#{@update}?conf=OK">#{navi_preference}</a></span>\n] if /^(latest|month|day|comment|conf|nyear|category.*)$/ !~ @mode
 	result
 end
