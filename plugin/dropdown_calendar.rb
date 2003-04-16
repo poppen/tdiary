70556fdabd9f43f72900c7e76cc277a4f01f38da
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2003-04-16 04:10:54 +0000

    * dropdown_calendar: fix p.calendar -> div.calendar. 	* dropdown_calendar: support @options['dropdown_calendar.label'].

--- plugin/dropdown_calendar.rb
+++ plugin/dropdown_calendar.rb
@@ -1,11 +1,11 @@
-# dropdown_calendar2.rb $Revision: 1.1 $
+# dropdown_calendar2.rb $Revision: 1.2 $
 #
 # calendar: カレンダーをドロップダウンリストに置き換えるプラグイン
 #   パラメタ: なし
 #
 def calendar
 	result = %Q[<form method="get" action="#{@index}">\n]
-	result << %Q[<p class="calendar">過去の日記\n]
+	result << %Q[<div class="calendar">#{@options['dropdown_calendar.label'] || '過去の日記'}\n]
 	result << %Q[<select name="date">\n]
 	@years.keys.sort.reverse_each do |year|
 		@years[year.to_s].sort.reverse_each do |month|
@@ -14,5 +14,5 @@ def calendar
 	end
 	result << "</select>\n"
 	result << %Q[<input type="submit" value="Go">\n]
-	result << "</p>\n</form>"
+	result << "</div>\n</form>"
 end
