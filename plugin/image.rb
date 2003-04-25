22947647b600a2cc31ba7b5a1f617dcefb3582d2
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2003-04-25 03:30:29 +0000

    * image: maxnum and maxsize effective in secure mode. 	* image: add JavaScript for insert plugin tag into diary.

--- plugin/image.rb
+++ plugin/image.rb
@@ -1,4 +1,4 @@
-# image.rb $Revision: 1.4 $
+# image.rb $Revision: 1.5 $
 # -pv-
 # 
 # 名称:
@@ -27,12 +27,11 @@
 #  @options['image.url']
 #     画像ファイルを保存するディレクトリのURL。無指定時は'./images/'
 #  @options['image.maxnum']
-#     1日あたりの最大画像数。無指定時は10
+#     1日あたりの最大画像数。無指定時は1
+#     ただし@secure = true時のみ有効
 #  @options['image.maxsize']
-#     1枚あたりの最大画像バイト数。無指定時は512000
-#
-# @secure = trueな環境で動かす場合は、必ずmaxnumとmaxsizeを適切な
-# 値に制限することを推奨します。
+#     1枚あたりの最大画像バイト数。無指定時は10000
+#     ただし@secure = true時のみ有効
 #
 # ライセンスについて:
 # Copyright (c) 2002,2003 Daisuke Kato <dai@kato-agri.com>
@@ -41,6 +40,12 @@
 # Distributed under the GPL
 
 =begin Changelog
+2003-04-25 TADA Tadashi <sho@spc.gr.jp>
+	* maxnum and maxsize effective in secure mode.
+
+2003-04-23 Yoshimi KURUMA <yoshimik@iris.dti.ne.jp>
+	* add JavaScript for insert plugin tag into diary.
+
 2003-04-24 TADA Tadashi <sho@spc.gr.jp>
 	* enable running on secure mode.
 
@@ -114,8 +119,8 @@ if @conf.secure and /^(form|edit|formplugin)$/ =~ @mode then
 end
 
 if /^formplugin$/ =~ @mode then
-   maxnum = @options['image.maxnum'] || 10
-   maxsize = @options['image.maxsize'] || 512000
+   maxnum = @options['image.maxnum'] || 1
+   maxsize = @options['image.maxsize'] || 10000
 
    date = @date.strftime( "%Y%m%d" )
 	images = image_list( date )
@@ -128,7 +133,7 @@ if /^formplugin$/ =~ @mode then
 			rescue NameError
          	size = @cgi.params['plugin_image_file'][0].stat.size
 			end
-         if images.compact.length < maxnum and size <= maxsize then
+         if not @conf.secure or (images.compact.length < maxnum and size <= maxsize) then
             file = "#{@image_dir}/#{date}_#{images.length}.#{extension}".untaint
 	         File::umask( 022 )
 	         File::open( file, "wb" ) do |f|
@@ -152,6 +157,39 @@ add_form_proc do |date|
 	r = ''
 	images = image_list( date.strftime( '%Y%m%d' ) )
 	if images.length > 0 then
+		r << %Q[
+		<script type="text/javascript">
+		<!--
+		var elem=null
+		function ins(val){
+			elem.value+=val
+		}
+		window.onload=function(){
+			for(var i=0;i<document.forms.length;i++){
+				for(var j=0;j<document.forms[i].elements.length;j++){
+					var e=document.forms[i].elements[j]
+					if(e.type&&e.type=="textarea"){
+						if(elem==null){
+							elem=e
+						}
+						e.onfocus=new Function("elem=this")
+					}
+				}
+			}
+		}
+		//-->
+		</script>
+		]
+		if @conf.style == "Wiki"
+			ptag1 = "{{"
+			ptag2 = "}}"
+		elsif @conf.style == "RD"
+			ptag1 = "((%"
+			ptag2 = "%))"
+		else
+			ptag1 = "&lt;%="
+			ptag2 = "%&gt;"
+		end
 	   r << %Q[<div class="form">
 		<div class="caption">
 		絵日記(一覧・削除)
@@ -164,13 +202,18 @@ add_form_proc do |date|
 	   end
 		r << "</tr><tr>"
 	   images.each_with_index do |img,id|
-	      r << %Q[<td><input type="checkbox" name="plugin_image_id" value="#{id}">#{id}</td>] if img
+			next unless img
+			ptag = "#{ptag1}image #{id}, '画像の説明'#{ptag2}"
+	      r << %Q[<td>
+			<input type="checkbox" name="plugin_image_id" value="#{id}">#{id}
+			<input type="button" onclick="ins(&quot;#{ptag}&quot;)" value="本文に追加">
+			</td>]
 	   end
 	   r << %Q[</tr>
 		</table>
 		<input type="hidden" name="plugin_image_delimage" value="true">
 	   <input type="hidden" name="date" value="#{date.strftime( '%Y%m%d' )}">
-	   <input type="submit" name="plugin" value="選択した画像ファイルの削除">
+	   <input type="submit" name="plugin" value="チェックした画像の削除">
 	   </div></form>
 		</div>]
 	end
@@ -183,7 +226,7 @@ add_form_proc do |date|
    <input type="hidden" name="plugin_image_addimage" value="true">
    <input type="hidden" name="date" value="#{date.strftime( '%Y%m%d' )}">
    <input type="file"	name="plugin_image_file">
-   <input type="submit" name="plugin" value="画像ファイルの追加">
+   <input type="submit" name="plugin" value="画像の追加">
    </div></form>
 	</div>]
 end
