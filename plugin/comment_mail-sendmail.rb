0cf8e30c4095d0fe4b6468f031b0ea25fb5d6453
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2003-06-12 09:20:12 +0000

    * [TESTING] comment_mail-*.rb: support conf_proc.

--- plugin/comment_mail-sendmail.rb
+++ plugin/comment_mail-sendmail.rb
@@ -1,30 +1,36 @@
-# comment_mail_sendmail.rb $Revision: 1.2 $
+# comment_mail_sendmail.rb $Revision: 1.3 $
 #
 # sendmailを使ってツッコミをメールで知らせる
 #   入れるだけで動作する。
 #
 # Options:
-#   @options['comment_mail.header']
-#        メールのSubjectに使う文字列。振り分け等に便利なように指定する。
-#        実際のSubjectは「指定文字列:日付-1」のように、日付とコメント番号が
-#        付く。ただし指定文字列中に、%に続く英字があった場合、それを
-#        日付フォーマット指定を見なす。つまり「日付」の部分は
-#        自動的に付加されなくなる(コメント番号は付加される)。
-#        無指定時には空文字。
-#   @options['comment_mail.receivers']
-#        メールを送るアドレスの配列。無指定時には日記筆者のアドレスになる。
-#   @options['comment_mail.sendmail']
-#        sendmailコマンドのパスを指定する。
-#        無指定時には「'/usr/sbin/sendmail'」。
+#   設定画面から指定できるもの(ツッコミメール系プラグイン共通):
+#     @options['comment_mail.enable']
+#          メールを送るかどうかを指定する。true(送る)かfalse(送らない)。
+#          無指定時はfalse。
+#     @options['comment_mail.header']
+#          メールのSubjectに使う文字列。振り分け等に便利なように指定する。
+#          実際のSubjectは「指定文字列:日付-1」のように、日付とコメント番号が
+#          付く。ただし指定文字列中に、%に続く英字があった場合、それを
+#          日付フォーマット指定を見なす。つまり「日付」の部分は
+#          自動的に付加されなくなる(コメント番号は付加される)。
+#          無指定時には空文字。
+#     @options['comment_mail.receivers']
+#          メールを送るアドレス文字列。カンマで区切って複数指定できる。
+#          無指定時には日記筆者のアドレスになる。
+#  
+#   tdiary.confでのみ指定できるもの:
+#     @options['comment_mail.sendmail']
+#          sendmailコマンドのパスを指定する。
+#          無指定時には「'/usr/sbin/sendmail'」。
 #
 # Copyright (c) 2003 TADA Tadashi <sho@spc.gr.jp>
 # You can distribute this file under the GPL.
 #
-def comment_mail( text )
+def comment_mail( text, to )
 	begin
-		sendmail = @options['comment_mail.sendmail'] || '/usr/sbin/sendmail'
-		receivers = @options['comment_mail.receivers']
-		open( "|#{sendmail} #{receivers.join(' ')}", 'w' ) do |o|
+		sendmail = @conf.options['comment_mail.sendmail'] || '/usr/sbin/sendmail'
+		open( "|#{sendmail} #{to.join(' ')}", 'w' ) do |o|
 			o.write( text )
 		end
 	rescue
@@ -32,6 +38,13 @@ def comment_mail( text )
 	end
 end
 
-if (@mode == 'comment' or @mode == 'trackbackreceive') and @comment then
+if @mode =~ /^(comment|trackbackreceive)$/ then
 	comment_mail_send
+end
+
+if @mode =~ /^(saveconf|conf)$/ then
+	add_conf_proc( 'comment_mail', 'ツッコミメール' ) do
+		comment_mail_basic_setting if @mode == 'saveconf'
+		comment_mail_basic_html
+	end
 end
--- plugin/comment_mail-smtp.rb
+++ plugin/comment_mail-smtp.rb
@@ -1,42 +1,53 @@
-# comment_mail-smtp.rb $Revision: 1.3 $
+# comment_mail-smtp.rb $Revision: 1.4 $
 #
 # SMTPプロトコルを使ってツッコミをメールで知らせる
 #   入れるだけで動作する
 #
 # Options:
-#   @options['comment_mail.header']
-#        メールのSubjectに使う文字列。振り分け等に便利なように指定する。
-#        実際のSubjectは「指定文字列:日付-1」のように、日付とコメント番号が
-#        付く。ただし指定文字列中に、%に続く英字があった場合、それを
-#        日付フォーマット指定を見なす。つまり「日付」の部分は
-#        自動的に付加されなくなる(コメント番号は付加される)。
-#        無指定時には空文字。
-#   @options['comment_mail.receivers']
-#        メールを送るアドレスの配列。無指定時には日記筆者のアドレスになる。
-#   @options['comment_mail.smtp_host']
-#   @options['comment_mail.smtp_port']
-#        それぞれ、メール送信に使うSMTPサーバのホスト名とポート番号。
-#        無指定時はそれぞれ「'localhost'「と「25」。
+#   設定画面から指定できるもの(ツッコミメール系プラグイン共通):
+#     @options['comment_mail.enable']
+#          メールを送るかどうかを指定する。true(送る)かfalse(送らない)。
+#          無指定時はfalse。
+#     @options['comment_mail.header']
+#          メールのSubjectに使う文字列。振り分け等に便利なように指定する。
+#          実際のSubjectは「指定文字列:日付-1」のように、日付とコメント番号が
+#          付く。ただし指定文字列中に、%に続く英字があった場合、それを
+#          日付フォーマット指定を見なす。つまり「日付」の部分は
+#          自動的に付加されなくなる(コメント番号は付加される)。
+#          無指定時には空文字。
+#     @options['comment_mail.receivers']
+#          メールを送るアドレス文字列。カンマで区切って複数指定できる。
+#          無指定時には日記筆者のアドレスになる。
+#  
+#   tdiary.confでのみ指定できるもの:
+#     @options['comment_mail.smtp_host']
+#     @options['comment_mail.smtp_port']
+#          それぞれ、メール送信に使うSMTPサーバのホスト名とポート番号。
+#          無指定時はそれぞれ「'localhost'」と「25」。
 #
 # Copyright (c) 2003 TADA Tadashi <sho@spc.gr.jp>
 # You can distribute this file under the GPL.
 #
-def comment_mail( text )
+def comment_mail( text, to )
 	begin
 		require 'net/smtp'
-		host = @options['comment_mail.smtp_host'] || 'localhost'
-		port = @options['comment_mail.smtp_port'] || 25
+		host = @conf.options['comment_mail.smtp_host'] || 'localhost'
+		port = @conf.options['comment_mail.smtp_port'] || 25
 		Net::SMTP.start( host, port ) do |smtp|
-			smtp.send_mail( text, @conf.author_mail, @options['comment_mail.receivers'] )
+			smtp.send_mail( text, @conf.author_mail, to )
 		end
 	rescue
 		$stderr.puts $!
 	end
 end
 
-if (@mode == 'comment' or @mode == 'trackbackreceive') and @comment then
-	@options['comment_mail.smtp_host'] ||= @conf.smtp_host || 'localhost'
-	@options['comment_mail.smtp_port'] ||= @conf.smtp_port || 25
+if @mode =~ /^(comment|trackbackreceive)$/ then
 	comment_mail_send
 end
 
+if @mode =~ /^(saveconf|conf)$/ then
+	add_conf_proc( 'comment_mail', 'ツッコミメール' ) do
+		comment_mail_basic_setting if @mode == 'saveconf'
+		comment_mail_basic_html
+	end
+end
