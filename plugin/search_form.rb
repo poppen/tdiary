bd1c4d2e3190a55062bc782537dbca282e08ede2
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2003-08-20 09:02:08 +0000

    * search_form.rb: to valid HTML. thanks Kakutani.

--- plugin/search_form.rb
+++ plugin/search_form.rb
@@ -1,4 +1,4 @@
-# search_form.rb $Revision: 1.5 $
+# search_form.rb $Revision: 1.6 $
 # -pv-
 #
 # 名称：
@@ -39,34 +39,16 @@
 # Copyright (c) 2002 MUTOH Masao <mutoh@highway.ne.jp>
 # Distributed under the same license terms as tDiary.
 # 
-=begin ChangeLog
-2002-10-13 MUTOH Masao <mutoh@highway.ne.jp>
-	* Google検索で検索結果が文字化けする不具合の修正
-	* Yahoo検索で検索ボックスの左側に画像を表示するようにした
-	* Lycos検索削除（日本語の文字化けへの対応方法がわからなかったため）
-     All of them were pointed out by patagon.
-	* version 1.0.2
-
-2002-05-19 MUTOH Masao <mutoh@highway.ne.jp>
-	* ドキュメントアップデート
-	* version 1.0.1
-
-2002-04-01 MUTOH Masao <mutoh@highway.ne.jp>
-	* tab = 3, 文書の体裁を整えた
-
-2002-03-24 MUTOH Masao <mutoh@highway.ne.jp>
-	* Namazu, Google, Yahoo!, Lycosの検索フォームをサポート
-	* version 1.0.0
-=end
-
 def search_form(url, query, button_name = "Search", size = 20, 
 						default_text = "", first_form = "", last_form = "")
 %Q[
 	<form class="search" method="GET" action="#{url}">
+	<div class="search">
 	#{first_form}
 		<input class="search" type="text" name="#{query}" size="#{size}" value="#{default_text}">
 		<input class="search" type="submit" value="#{button_name}">
 	#{last_form}
+	</div>
 	</form>
 ]
 end
@@ -78,7 +60,7 @@ end
 def googlej_form(button_name = "Google 検索", size = 20, default_text = "")
 	first = %Q[<a href="http://www.google.com/">
 		<img src="http://www.google.com/logos/Logo_40wht.gif" 
-			border="0" alt="Google" align="absmiddle"></a>]
+			style="border-width: 0px; vertical-align: middle;" alt="Google"></a>]
 	last = %Q[<input type=hidden name=hl value="ja"><input type=hidden name=ie value="euc-jp">]
 	search_form("http://www.google.com/search", "q", button_name, size, default_text, first, last)
 end
@@ -86,6 +68,6 @@ end
 def yahooj_form(button_name = "Yahoo! 検索", size = 20, default_text = "")
 	first = %Q[<a href="http://www.google.com/">
 		<img src="http://img.yahoo.co.jp/images/yahoojp_sm.gif" 
-			border="0" alt="Yahoo! JAPAN" align="absmiddle"></a>]
+			style="border-width: 0px; vertical-align: middle;" alt="Yahoo! JAPAN"></a>]
 	search_form("http://search.yahoo.co.jp/bin/search", "p", button_name, size, default_text, first, "")
 end
