dd68f02d3a270eedf6bd05b800d07adbbfed5817
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2003-10-09 02:50:21 +0000

    * referer-antibot.rb: support 'bot?' method and English document.

--- plugin/referer-antibot.rb
+++ plugin/referer-antibot.rb
@@ -1,46 +1,45 @@
-# referer-antibot.rb $Revision: 1.1 $
+# referer-antibot.rb $Revision: 1.2 $
 #
 # 検索エンジンの巡回BOTには「本日のリンク元」を見せないようにする
 # これにより、無関係な検索語でアクセスされることが減る(と予想される)
 # pluginディレクトリに入れるだけで動作する
 #
 # オプション:
-#   @options['disp_referrer.deny_user_agents']
-#      ターゲットにする巡回BOTのuser agentを追加する配列。
-#      このオプションはdisp_referrerプラグインと共通。
+#   @options['bot']
+#      ターゲットにする巡回BOTのUser-Agentを追加する配列。
 #      無指定時は["googlebot", "Hatena Antenna", "moget@goo.ne.jp"]のみ。
 #
 # なお、disp_referrer.rbプラグインには同等の機能が含まれているので、
 # disp_referrerを導入済みの場合には入れる必要はない
 #
+# ---------
+#
+# This plugin hide Today's Link to search engin's robots.
+# It may reduce nose marked by robots.
+#
+# disp_referrer.rb plugin has already this function. You don't
+# need install this plugin with disp_referrer.rb.
+#
+# Options:
+#    @options['bot']
+#      An array of User-Agent of search engine's robots.
+#      Default setting is ["googlebot", "Hatena Antenna", "moget@goo.ne.jp"].
+#
 # Copyright (C) 2002 MUTOH Masao <mutoh@highway.ne.jp>
 # Modified by TADA Tadashi <sho@spc.gr.jp>
 # You can redistribute it and/or modify it under GPL2.
 #
-=begin ChangeLog
-2003-03-28 TADA Tadashi <sho@spc.gr.jp>
-	* modify disp_referer.rb.
-=end
-
-# deny user agents
-deny_user_agents = ["googlebot", "Hatena Antenna", "moget@goo.ne.jp"]
-deny_user_agents += @options['disp_referrer.deny_user_agents'] || []
-@referer_antibots = Regexp::new( "(#{deny_user_agents.join( '|' )})" )
-
-def referer_antibot?
-	@referer_antibots =~ @cgi.user_agent
-end
 
 # short referer
 alias referer_of_today_short_antibot_backup referer_of_today_short
 def referer_of_today_short( diary, limit )
-	return '' if referer_antibot?
+	return '' if bot?
 	referer_of_today_short_antibot_backup( diary, limit )
 end
 
 # long referer
 alias referer_of_today_long_antibot_backup referer_of_today_long
 def referer_of_today_long( diary, limit )
-	return '' if referer_antibot?
+	return '' if bot?
 	referer_of_today_long_antibot_backup( diary, limit )
 end
