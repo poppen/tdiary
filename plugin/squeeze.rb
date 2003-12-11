e831b6158dbca79a4063067052f366046f481f40
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2003-12-11 09:07:51 +0000

    * squeeze.rb: behave to like a bot.

--- plugin/squeeze.rb
+++ plugin/squeeze.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# squeeze.rb $Revision: 1.14 $
+# squeeze.rb $Revision: 1.15 $
 #
 # Create daily HTML file from tDiary database.
 #
@@ -127,7 +127,7 @@ if mode == "CMD" || mode == "CGI"
 
 	if mode == "CMD"
 		def usage
-			puts "squeeze $Revision: 1.14 $"
+			puts "squeeze $Revision: 1.15 $"
 			puts " making html files from tDiary's database."
 			puts " usage: ruby squeeze.rb [-p <tDiary path>] [-c <tdiary.conf path>] [-a] [-s] [-x suffix] <dest path>"
 			exit
@@ -279,7 +279,7 @@ if mode == "CGI" || mode == "CMD"
 			</head>
 			<body><div style="text-align:center">
 			<h1>Squeeze for tDiary</h1>
-			<p>$Revision: 1.14 $</p>
+			<p>$Revision: 1.15 $</p>
 			<p>Copyright (C) 2002 MUTOH Masao&lt;mutoh@highway.ne.jp&gt;</p></div>
 			<br><br>Start!</p><hr>
 		]
@@ -290,8 +290,8 @@ if mode == "CGI" || mode == "CMD"
 		conf.header = ''
 		conf.footer = ''
 		conf.show_comment = true
-		conf.show_referer = false
 		conf.hide_comment_form = true
+		def conf.bot?; true; end
 		output_path = "#{conf.data_path}/cache/html" unless output_path
 		Dir.mkdir(output_path, 0755) unless File.directory?(output_path)
 		TDiary::YATDiarySqueezeMain.new(output_path, all_data, compat, conf, suffix)
@@ -313,8 +313,8 @@ elsif /^(append|replace|comment|trackbackreceive)$/ =~ @mode
 	conf.header = ''
 	conf.footer = ''
 	conf.show_comment = true
-	conf.show_referer = false
 	conf.hide_comment_form = true
+	def conf.bot?; true; end
 
 	diary = @diaries[@date.strftime('%Y%m%d')]
 	dir = @options['squeeze.output_path'] || @options['yasqueeze.output_path']
