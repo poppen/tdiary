9d5bae402d5a831707a906caf61a91660eb3a5b4
zunda <zunda@7f22e88f-374d-0410-998f-c91420d97ba2>
2003-12-17 13:12:17 +0000

    referer_scheme.rb: avoid finite call of aliased `each' when the plug-in is eval'ed more than once.

--- plugin/referer_scheme.rb
+++ plugin/referer_scheme.rb
@@ -1,5 +1,5 @@
 =begin
-= Meta-scheme plugin((-$Id: referer_scheme.rb,v 1.2 2003-12-16 18:32:41 zunda Exp $-))
+= Meta-scheme plugin((-$Id: referer_scheme.rb,v 1.3 2003-12-17 13:12:17 zunda Exp $-))
 Enables to prefix `meta' schemes to URL regexp of the refer_table. See
 #{lang}/referer_scheme.rb for a documentation.
 
@@ -11,19 +11,21 @@ distribution of modified versions of this work under the terms of GPL
 version 2 or later.
 =end
 
-class << @conf.referer_table
+unless @conf.referer_table.respond_to?( 'referer_scheme_each_orig' ) then
 
-	# expands referer_table according to the meta-scheme
-	alias referer_scheme_each_orig each
-	def each
-		self.referer_scheme_each_orig do |url, name|
-			/^(\w+):/ =~ url
-			if $1 && self.respond_to?( "scheme_#{$1}", true ) then
-				self.send( "scheme_#{$1}", $', name ) do |expanded_url, expanded_name|
-					yield( expanded_url, expanded_name )
+	class << @conf.referer_table
+		# expands referer_table according to the meta-scheme
+		alias referer_scheme_each_orig each
+		def each
+			self.referer_scheme_each_orig do |url, name|
+				/^(\w+):/ =~ url
+				if $1 && self.respond_to?( "scheme_#{$1}", true ) then
+					self.send( "scheme_#{$1}", $', name ) do |expanded_url, expanded_name|
+						yield( expanded_url, expanded_name )
+					end
+				else
+					yield( url, name )
 				end
-			else
-				yield( url, name )
 			end
 		end
 	end
