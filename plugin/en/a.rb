15ee9483f91cd9180fd1fe3a67a83a2a8187838d
mutoh <mutoh@7f22e88f-374d-0410-998f-c91420d97ba2>
2004-09-26 01:44:03 +0000

    * plugin/a.rb: Support conf menu.  * plugin/a/: Removed.  * plugin/en/a.rb: Added.

---
+++ plugin/en/a.rb
@@ -0,0 +1,3 @@
+def a_conf_label; "Edit the dictionary file for the anchor plugin(a.rb)"; end
+def a_conf_explain; "<p>A row is an anchor. The format is: key URL name. name is omittable. If you omit name, the key used as name.</p><p>e.g.) bibo http://www.tdiary.org/ tDiary Website</p>"; end
+
