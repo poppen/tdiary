1707a78411f534ae9d49d6370373cf02490e800b
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2005-05-27 13:07:55 +0000

    * add new plugin: add_bookmark.

---
+++ contrib/plugin/add_bookmark/ja/add_bookmark.rb
@@ -0,0 +1,11 @@
+# Japanese resource of add_bookmark.rb
+#
+
+@add_bookmark_label = '追加ブックマーク設定'
+@add_bookmark_desc = 'ページに埋め込みたいリンクをチェックしてください。'
+@caption_hatena = 'はてなブックマークに追加'
+@caption_del = 'del.icio.usに追加'
+@caption_mm = 'MM/Memoに追加'
+@label_hatena = 'はてなブックマーク'
+@label_del = 'del.icio.us'
+@label_mm = 'MM/Memo'
