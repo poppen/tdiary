c39db3c401270692c37cffc22d3199026beb17d3
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2005-06-04 11:46:59 +0000

    * navi_user.rb: support 'n days before/after' mode. 	* navi_user.rb: change implementation to overwrite navi_user_day.

--- plugin/navi_user.rb
+++ plugin/navi_user.rb
@@ -1,4 +1,4 @@
-# navi_user.rb $Revision: 1.6 $
+# navi_user.rb $Revision: 1.7 $
 #
 # navi_user: 前日，翌日→前の日記，次の日記
 #   modeがday/commentのときに表示される「前日」「翌日」ナビゲーション
@@ -18,52 +18,35 @@ module TDiary
 end
 MODIFY_CLASS
 
-def navi_user
-	result = navi_item( @index_page, navi_index ) unless @index_page.empty?
+def navi_user_day
+	cgi = CGI.new
+	def cgi.referer; nil; end
+	days = []
+	yms = []
+	today = @date.strftime('%Y%m%d')
+	this_month = @date.strftime('%Y%m')
 
-	case @mode
-	when 'latest'
-		result << navi_item( "#{@index}#{anchor( @conf['ndays.prev'] + '-' + @conf.latest_limit.to_s )}", "&laquo;#{navi_prev_ndays}" ) if @conf['ndays.prev']
-		result << navi_item( @index, navi_latest ) if @cgi.params['date'][0]
-		result << navi_item( "#{@index}#{anchor( @conf['ndays.next'] + '-' + @conf.latest_limit.to_s )}", "#{navi_next_ndays}&raquo;" ) if @conf['ndays.next']
-	when 'day'
-		cgi = CGI.new
-		def cgi.referer; nil; end
-		days = []
-		yms = []
-		today = @date.strftime('%Y%m%d')
-		this_month = @date.strftime('%Y%m')
-
-		@years.keys.each do |y|
-			yms += @years[y].collect {|m| y + m}
-		end
-		yms |= [this_month]
-		yms.sort!
-		yms.unshift(nil).push(nil)
-		yms[yms.index(this_month) - 1, 3].each do |ym|
-			next unless ym
-			cgi.params['date'] = [ym]
-			m = TDiaryMonth.new(cgi, '', @conf)
-			days += m.diaries.keys.sort
-		end
-		days |= [today]
-		days.sort!
-		days.unshift(nil).push(nil)
-		prev_day, cur_day, next_day = days[days.index(today) - 1, 3]
-		if prev_day
-			result << navi_item( "#{@index}#{anchor prev_day}", "&laquo;#{navi_prev_diary(navi_user_format(prev_day))}" )
-		end
-		result << navi_item( @index, navi_latest )
-		if next_day
-			result << navi_item( "#{@index}#{anchor next_day}", "#{navi_next_diary(navi_user_format(next_day))}&raquo;" )
-		end
-	when 'nyear'
-		result << navi_item( "#{@index}#{anchor @prev_day[4,4]}", "&laquo;#{navi_prev_nyear Time::local(*@prev_day.scan(/^(\d{4})(\d\d)(\d\d)$/)[0])}" ) if @prev_day     
-		result << navi_item( @index, navi_latest ) 
-		result << navi_item( "#{@index}#{anchor @next_day[4,4]}", "#{navi_next_nyear Time::local(*@next_day.scan(/^(\d{4})(\d\d)(\d\d)$/)[0])}&raquo;" ) if @next_day
-	else
-		result << navi_item( @index, navi_latest )
+	@years.keys.each do |y|
+		yms += @years[y].collect {|m| y + m}
 	end
+	yms |= [this_month]
+	yms.sort!
+	yms.unshift(nil).push(nil)
+	yms[yms.index(this_month) - 1, 3].each do |ym|
+		next unless ym
+		cgi.params['date'] = [ym]
+		m = TDiaryMonth.new(cgi, '', @conf)
+		days += m.diaries.keys.sort
+	end
+	days |= [today]
+	days.sort!
+	days.unshift(nil).push(nil)
+	prev_day, cur_day, next_day = days[days.index(today) - 1, 3]
+
+	result = ''
+	result << navi_item( "#{@index}#{anchor prev_day}", "&laquo;#{navi_prev_diary(navi_user_format(prev_day))}" ) if prev_day
+	result << navi_item( @index, navi_latest )
+	result << navi_item( "#{@index}#{anchor next_day}", "#{navi_next_diary(navi_user_format(next_day))}&raquo;" ) if next_day
 	result
 end
 
