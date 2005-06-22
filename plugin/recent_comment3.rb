b996588ea65a8de667ca916fcc3a58543a42b190
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2005-06-22 06:56:41 +0000

    * recent_comment3.rb: add a configuration page, and delete all parameters.

--- plugin/recent_comment3.rb
+++ plugin/recent_comment3.rb
@@ -1,10 +1,5 @@
-# $Revision: 1.17 $
+# $Revision: 1.18 $
 # recent_comment3: 最近のツッコミをリストアップする
-#   パラメタ:
-#     max:           最大表示数(未指定時:3)
-#     sep:           nil
-#     date_format:   日付のフォーマット(未指定時:日記の日付表記+「時:分」)
-#     except:        無視する名前(いくつもある場合は,で区切って並べる)
 #
 #   @secure = true な環境では動作しません．
 #
@@ -13,64 +8,102 @@
 #
 require 'pstore'
 
-RECENT_COMMENT3_CACHE = "#{@cache_path}/recent_comments"
-RECENT_COMMENT3_NUM = 50
+def recent_comment3_format(format, *args)
+	format.gsub(/\$(\d)/) {|s| args[$1.to_i - 1]}
+end
 
-def recent_comment3(max = 3, sep = 'OBSOLUTE', date_format = "(#{@date_format + ' %H:%M'})", *except )
-	date_format = "(#{@date_format + ' %H:%M'})" unless date_format.respond_to?(:to_str)
-	result = []
-	idx = 0
-	PStore.new(RECENT_COMMENT3_CACHE).transaction do |db|
-		break unless db.root?('comments')
-		db['comments'].each do |c|
-			break if idx >= max or c.nil?
-			comment, date, serial = c
-			next unless comment.visible?
-			next if except.include?(comment.name)
-			str = %Q|<li><a href="#{@index}#{anchor date.strftime('%Y%m%d')}#c#{'%02d' % serial}" title="#{CGI::escapeHTML(comment.shorten( @conf.comment_length ))}">#{CGI::escapeHTML(comment.name)}#{comment.date.strftime(date_format)}</a></li>\n|
-			result << str
-			idx += 1
-		end
-	end
-	if result.size == 0
-		''
-	else
-		%Q|<ol class="recent-comment">\n| + result.join( '' ) + "</ol>\n"
-	end
+def recent_comment3_init
+   @conf['recent_comment3.cache'] ||= "#{@cache_path}/recent_comments"
+   @conf['recent_comment3.cache_size'] ||= 50
+   @conf['recent_comment3.max'] ||= 3
+   @conf['recent_comment3.date_format'] ||= "(%m-%d)"
+   @conf['recent_comment3.except_list'] ||= ''
+	@conf['recent_comment3.format'] ||= '<a href="$2" title="$3">$4 $5</a>'
+end
+
+def recent_comment3(ob_max = 'OBSOLUTE' ,sep = 'OBSOLUTE',ob_date_format = 'OBSOLUTE',*ob_except )
+   recent_comment3_init
+   
+   cache = @conf['recent_comment3.cache']
+   max = @conf['recent_comment3.max']
+   date_format = @conf['recent_comment3.date_format'] 
+   except = @conf['recent_comment3.except_list'].split(/,/)
+	format = @conf['recent_comment3.format']
+   
+   result = []
+   idx = 0
+   PStore.new(cache).transaction do |db|
+      break unless db.root?('comments')
+      db['comments'].each do |c|
+         break if idx >= max or c.nil?
+         comment, date, serial = c
+         next unless comment.visible?
+         next if except.include?(comment.name)
+         a = @index + anchor("#{date.strftime('%Y%m%d')}#c#{'%02d' % serial}")
+         popup = CGI::escapeHTML(comment.shorten( @conf.comment_length ))
+         str = CGI::escapeHTML(comment.name)
+         date_str = comment.date.strftime(date_format)
+
+         idx += 1
+
+         result << "<li>"
+			result << recent_comment3_format(format, idx, a, popup, str, date_str)
+			result << "</li>\n"
+      end
+		db.abort
+   end
+   if result.size == 0
+      ''
+   else
+      %Q|<ol class="recent-comment">\n| + result.join( '' ) + "</ol>\n"
+   end
 end
 
 add_update_proc do
-	date = @date.strftime( '%Y%m%d' )
+   recent_comment3_init
+   
+   date = @date.strftime( '%Y%m%d' )
+   cache = @conf['recent_comment3.cache']
+   size = @conf['recent_comment3.cache_size']
+   
+   if @mode == 'comment' and @comment then
+      PStore.new( cache ).transaction do |db|
+         comment = @comment
+         serial = 0
+         @diaries[date].each_comment( 100 ) do
+            serial += 1
+         end
+         db['comments'] = Array.new( size ) unless db.root?( 'comments' )
+         if db['comments'][0].nil? or comment != db['comments'][0][0]
+            db['comments'].unshift([comment, @date, serial]).pop
+         end
+      end
+   elsif @mode == 'showcomment'
+      PStore.new( cache ).transaction do |db|
+         break unless db.root?('comments')
+         
+         @diaries[date].each_comment( 100 ) do |dcomment|
+            db['comments'].each do |c|
+               break if c.nil?
+               comment, cdate, serial = c
+               next if cdate.strftime('%Y%m%d') != date
+               if comment == dcomment and comment.date == dcomment.date
+                  comment.show = dcomment.visible?
+                  next
+               end
+            end
+         end
+      end
+   end
+end
 
-	if @mode == 'comment' and @comment then
-		PStore.new( RECENT_COMMENT3_CACHE ).transaction do |db|
-			comment = @comment
-			serial = 0
-			@diaries[date].each_comment( 100 ) do
-				serial += 1
-			end
-			db['comments'] = Array.new( RECENT_COMMENT3_NUM ) unless db.root?( 'comments' )
-			if db['comments'][0].nil? or comment != db['comments'][0][0]
-				db['comments'].unshift([comment, @date, serial]).pop
-			end
-		end
-	elsif @mode == 'showcomment'
-		PStore.new( RECENT_COMMENT3_CACHE ).transaction do |db|
-			break unless db.root?('comments')
-	
-			@diaries[date].each_comment( 100 ) do |dcomment|
-				db['comments'].each do |c|
-					break if c.nil?
-					comment, cdate, serial = c
-					next if cdate.strftime('%Y%m%d') != date
-					if comment == dcomment and comment.date == dcomment.date
-						comment.show = dcomment.visible?
-						next
-					end
-				end
-			end
-		end
-	end
+if @mode == 'saveconf'
+   def saveconf_recent_comment3
+      @conf['recent_comment3.max'] = @cgi.params['recent_comment3.max'][0].to_i
+      @conf['recent_comment3.date_format'] = @cgi.params['recent_comment3.date_format'][0]
+      @conf['recent_comment3.except_list'] = @cgi.params['recent_comment3.except_list'][0]
+      @conf['recent_comment3.format'] = @cgi.params['recent_comment3.format'][0]
+   end
 end
 
 # vim: ts=3
---
+++ plugin/zh/recent_comment3.rb
@@ -0,0 +1,34 @@
+# en/recent_comment3.rb $Revision: 1.1 $
+#
+# English resources for recent_comment3.rb
+#
+# Copyright (c) 2005 Hiroshi SHIBATA <h-sbt@nifty.com>
+# Distributed under the GPL
+#
+
+if @mode == 'conf' || @mode == 'saveconf'
+   add_conf_proc( 'recent_comment3', 'Recent comment' ) do
+      saveconf_recent_comment3
+      recent_comment3_init
+      <<-HTML
+      <h3 class="subtitle">The number of display comment</h3>
+      <p>Max <input name="recent_comment3.max" value="#{@conf['recent_comment3.max']}" size="3" /> item</p>
+      <h3 class="subtitle">Date format</h3>
+      <p>Refer to <a href="http://www.ruby-lang.org/ja/man/index.cgi?cmd=view;name=Time#strftime">Ruby's Manual</a>for the \'%\' character that can be used.</p>
+      <p><input name="recent_comment3.date_format" value="#{CGI.escapeHTML(@conf['recent_comment3.date_format'])}" size="40" /></p>
+      <h3 class="subtitle">Non display name in recent comment list</h3>
+      <p>Specified The name not displayed in the list. Name is delimited by ","</p>
+      <p><input name="recent_comment3.except_list" size="60" value="#{CGI.escapeHTML( @conf['recent_comment3.except_list'] )}" /></p>
+      <h3 class="subtitle">HTML Template for generate</h3>
+      <p>Specify how each comment is rendered.</p>
+      <textarea name="recent_comment3.format" cols="70" rows="3">#{CGI.escapeHTML(@conf['recent_comment3.format'])}</textarea>
+      <p><em>$digit</em> in the template is replaced as follows.</p>
+      <dl>
+      <dt>$2</dt><dd>comment's URL</dd>
+      <dt>$3</dt><dd>comment's shortening display</dd>
+      <dt>$4</dt><dd>name of comment's author</dd>
+      <dt>$5</dt><dd>when the comment is received</dd>
+      </dl>
+      HTML
+   end
+end
