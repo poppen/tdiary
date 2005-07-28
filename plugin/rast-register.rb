40f495aa817f8d8e40a29cb8ec7afb85ac74c88f
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2005-07-28 04:44:43 +0000

    * rast-register.rb: add license.

--- plugin/rast-register.rb
+++ plugin/rast-register.rb
@@ -1,4 +1,8 @@
 #!/usr/bin/env ruby
+#
+# Copyright (C) 2005 Kazuhiko <kazuhiko@fdiary.net>
+# You can redistribute it and/or modify it under GPL2.
+#
 require 'rast'
 require 'htree'
 
@@ -19,7 +23,7 @@ if mode == "CMD" || mode == "CGI"
 
 	if mode == "CMD"
 		def usage
-			puts "rast-register.rb $Revision: 1.8 $"
+			puts "rast-register.rb $Revision: 1.9 $"
 			puts " register to rast index files from tDiary's database."
 			puts " usage: ruby rast-regiser.rb [-p <tDiary directory>] [-c <tdiary.conf directory>]"
 			exit
@@ -223,7 +227,7 @@ if mode == "CGI" || mode == "CMD"
 			</head>
 			<body><div style="text-align:center">
 			<h1>Rast Register for tDiary</h1>
-			<p>$Revision: 1.8 $</p>
+			<p>$Revision: 1.9 $</p>
 			<p>Copyright (C) 2005 Kazuhiko &lt;kazuhiko@fdiary.net &gt;<br>Copyright (C) 2002 MUTOH Masao &lt;mutoh@highway.ne.jp&gt;</p>
 			<p>Start!</p><hr>
 		]
