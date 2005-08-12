1d8ed21650cf14ee51d6fed341b3139e51eb39d9
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2005-08-12 02:11:34 +0000

    * plugin/account_ad: fix bug: delete blog parameter of add_header_proc. 	* plugin/account_ad: suppress embedding RDF when no account_name.

--- contrib/plugin/account_ad/account_ad.rb
+++ contrib/plugin/account_ad/account_ad.rb
@@ -1,10 +1,10 @@
-# account_ad.rb $Revision: 1.1 $
+# account_ad.rb $Revision: 1.2 $
 #
 # Copyright (c) 2005 SHIBATA  Hiroshi <h-sbt@nifty.com>
 # Distributed under the GPL
 #
 
-add_header_proc do |date|
+add_header_proc do
    account_ad_init
 
    account_name = @conf['account.name']
@@ -15,24 +15,28 @@ add_header_proc do |date|
    else
       permalink=@conf.base_url
    end
-   
-   <<-HTML
-<!--
-<rdf:RDF
-   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-   xmlns:foaf="http://xmlns.com/foaf/0.1/">
-<rdf:Description rdf:about="#{permalink}">
-   <foaf:maker rdf:parseType="Resource">
-     <foaf:holdsAccount>
-       <foaf:OnlineAccount foaf:accountName="#{account_name}">
-         <foaf:accountServiceHomepage rdf:resource="#{account_service}" />
-       </foaf:OnlineAccount>
-     </foaf:holdsAccount>
-   </foaf:maker>
-</rdf:Description>
-</rdf:RDF>
--->
-   HTML
+
+	if account_name.length > 0 then
+   	<<-HTML
+			<!--
+			<rdf:RDF
+			   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+			   xmlns:foaf="http://xmlns.com/foaf/0.1/">
+			<rdf:Description rdf:about="#{permalink}">
+			   <foaf:maker rdf:parseType="Resource">
+			     <foaf:holdsAccount>
+			       <foaf:OnlineAccount foaf:accountName="#{account_name}">
+			         <foaf:accountServiceHomepage rdf:resource="#{account_service}" />
+			       </foaf:OnlineAccount>
+			     </foaf:holdsAccount>
+			   </foaf:maker>
+			</rdf:Description>
+			</rdf:RDF>
+			-->
+   	HTML
+	else
+		''
+	end
 end
 
 def account_ad_init
