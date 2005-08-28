ab21f306256896c338e14506a11dccf429529125
hsbt <hsbt@7f22e88f-374d-0410-998f-c91420d97ba2>
2005-08-28 12:27:07 +0000

    * ja/recent_comment3.rb: add Tree view mode.

--- plugin/ja/recent_comment3.rb
+++ plugin/ja/recent_comment3.rb
@@ -1,4 +1,4 @@
-# ja/recent_comment3.rb $Revision: 1.3 $
+# ja/recent_comment3.rb $Revision: 1.4 $
 #
 # Japanese resources for recent_comment3.rb
 #
@@ -10,15 +10,27 @@ if @mode == 'conf' || @mode == 'saveconf'
    add_conf_proc( 'recent_comment3', '最近のツッコミ', 'tsukkomi' ) do
       saveconf_recent_comment3
       recent_comment3_init
+      checked = "t" == @conf['recent_comment3.tree'] ? ' checked' : ''
+
       <<-HTML
       <h3 class="subtitle">表示するツッコミの数</h3>
       <p>最大 <input name="recent_comment3.max" value="#{@conf['recent_comment3.max']}" size="3" /> 件</p>
+
       <h3 class="subtitle">日付フォーマット</h3>
       <p>使用できる\'%\'文字については<a href="http://www.ruby-lang.org/ja/man/index.cgi?cmd=view;name=Time#strftime">Rubyのマニュアル</a>を参照．</p>
       <p><input name="recent_comment3.date_format" value="#{CGI.escapeHTML(@conf['recent_comment3.date_format'])}" size="40" /></p>
+
       <h3 class="subtitle">一覧に表示しない名前</h3>
       <p>リストに表示しない名前を","で区切って指定します．</p>
       <p><input name="recent_comment3.except_list" size="60" value="#{CGI.escapeHTML( @conf['recent_comment3.except_list'] )}" /></p>
+
+      <h3 class="subtitle">ツリー表示機能</h3>
+      <p><input name="recent_comment3.tree" type="checkbox" value="t"#{checked} />ツリー表示機能を使用する</p>
+
+      <h3 class="subtitle">ツリー表示時のタイトルの長さ</h3>
+      <p>ツリー表示機能を使用する時のタイトルの長さを指定します。ツリー表示機能を使用しない場合には関係ありません。</p>
+      <p>最大 <input name="recent_comment3.titlelen" value="#{@conf['recent_comment3.titlelen']}" size="3" /> 文字</p>
+      
       <h3 class="subtitle">生成するHTMLのテンプレート</h3>
       <p>各ツッコミをどのようなHTMLで表示するかを指定します．</p>
       <textarea name="recent_comment3.format" cols="70" rows="3">#{CGI.escapeHTML(@conf['recent_comment3.format'])}</textarea>
