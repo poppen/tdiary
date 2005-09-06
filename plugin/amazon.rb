a1dcad8ee8ce80724d253e37a1b5d60f3efed3af
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2005-09-06 02:45:25 +0000

    * amazon.rb: deny zero length proxy.

--- plugin/amazon.rb
+++ plugin/amazon.rb
@@ -1,4 +1,4 @@
-# amazon.rb $Revision: 1.39 $: Making link with image to Amazon using Amazon ECS.
+# amazon.rb $Revision: 1.40 $: Making link with image to Amazon using Amazon ECS.
 #
 # see document: #{@lang}/amazon.rb
 #
@@ -28,8 +28,11 @@ def amazon_call_ecs( asin )
 	url << "&ResponseGroup=Medium"
 	url << "&Version=#{@amazon_require_version}"
 
-	proxy = @conf['amazon.proxy'] || nil
-	proxy = 'http://' + proxy if @conf['amazon.proxy']
+	proxy = nil
+	if @conf['amazon.proxy'] and @conf['amazon.proxy'].length > 0 then
+		proxy = @conf['amazon.proxy']
+		proxy = 'http://' + proxy unless proxy =~ /^https?:/
+	end
 
 	timeout( 10 ) do
 		open( url, :proxy => proxy ) {|f| f.read}
