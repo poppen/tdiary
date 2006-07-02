a70318098e051fa40b702555b38368e5be8a6ad4
hsbt <hsbt@7f22e88f-374d-0410-998f-c91420d97ba2>
2006-07-02 13:43:03 +0000

    * livedoor_wether.rb: change logic for lwws_get method. 	* weather_index.rb: add accessory cgi.

--- contrib/plugin/livedoor/livedoor_weather.rb
+++ contrib/plugin/livedoor/livedoor_weather.rb
@@ -11,194 +11,190 @@ require 'rexml/document'
 require 'nkf'
 
 def lwws_init
-   @conf['lwws.city_id'] ||= 63
-   @conf['lwws.icon.disp'] ||= ""
-   @conf['lwws.max_temp.disp'] ||= ""
-   @conf['lwws.min_temp.disp'] ||= ""
-   @conf['lwws.cache'] ||= ""
-   @conf['lwws.cache_time'] ||= 6
+	@conf['lwws.city_id'] ||= 63
+	@conf['lwws.icon.disp'] ||= ""
+	@conf['lwws.max_temp.disp'] ||= ""
+	@conf['lwws.min_temp.disp'] ||= ""
+	@conf['lwws.cache'] ||= ""
+	@conf['lwws.cache_time'] ||= 6
 end
 
 def convert_date( date_status )
   case date_status
-   when "today"
-      date = Time.now
-   when "tomorrow"
-      date = Time.now + (60 * 60 * 24)
-   when "dayaftertomorrow"
-      date = Time.now + (60 * 60 * 24 * 2)
-   end
-   return date.strftime("%Y%m%d")
+	when "today"
+		date = Time.now
+	when "tomorrow"
+		date = Time.now + (60 * 60 * 24)
+	when "dayaftertomorrow"
+		date = Time.now + (60 * 60 * 24 * 2)
+	end
+	return date.strftime("%Y%m%d")
 end
 
-
 def lwws_request( city_id, date_status )
-   
-   url =  @lwws_rest_url.dup
-   url << "?city=#{city_id}"
-   url << "&day=#{date_status}"
-   
-   proxy = nil
-   if @conf['amazon.proxy'] and @conf['amazon.proxy'].length > 0 then
-      proxy = @conf['amazon.proxy']
-      proxy = 'http://' + proxy unless proxy =~ /^https?:/
-   end
-   
-   timeout( 10 ) do
-      open( url, :proxy => proxy ) {|f| f.read}
-   end
+
+	url =  @lwws_rest_url.dup
+	url << "?city=#{city_id}"
+	url << "&day=#{date_status}"
+
+	proxy = nil
+	if @conf['amazon.proxy'] and @conf['amazon.proxy'].length > 0 then
+		proxy = @conf['amazon.proxy']
+		proxy = 'http://' + proxy unless proxy =~ /^https?:/
+	end
+
+	timeout( 10 ) do
+		open( url, :proxy => proxy ) {|f| f.read}
+	end
 end
 
 def lwws_get( date_status )
-   lwws_init
+	lwws_init
 
-   city_id = @conf["lwws.city_id"]
-   cache_time = @conf['lwws.cache_time'] * 60 * 60  # 6 hour
+	city_id = @conf['lwws.city_id']
+	cache_time = @conf['lwws.cache_time'] * 60 * 60  # 6 hour
 
-   begin
-      cache = "#{@cache_path}/lwws"
-      file_name = "#{cache}/#{convert_date( date_status )}.xml" # file_name is YYYYMMDD.xml
-      
-      Dir::mkdir( cache ) unless File::directory?( cache )
-      unless File::exist?( file_name )
-         xml =  lwws_request( city_id, date_status )
-         File::open( file_name, 'wb' ) {|f| f.write( xml )}
-      else
-         if @conf['lwws.cache'] == "t" && Time.now > File::stat( file_name ).mtime + cache_time
-            xml =  lwws_request( city_id, date_status )
-            File::open( file_name, 'wb' ) {|f| f.write( xml )}
-         end
-      end
-   rescue Timeout::Error
-      return
-   rescue NoMethodError
-      return
-   end
+	cache = "#{@cache_path}/lwws"
+	file_name = "#{cache}/#{convert_date( date_status )}.xml" # file_name is YYYYMMDD.xml
+
+	begin
+		Dir::mkdir( cache ) unless File::directory?( cache )
+		cached_time = nil
+		cached_time = File.mtime( file_name ) if File.exist?( file_name )
+
+		if cached_time.nil? or ( @conf['lwws.cache'] == "t" and Time.now > cached_time + cache_time )
+			xml =  lwws_request( city_id, date_status )
+			File::open( file_name, 'wb' ) {|f| f.write( xml )}
+		end
+	rescue Timeout::Error
+		return
+	rescue NoMethodError
+		return
+	end
 end
 
 def lwws_to_html( date_status, date = nil )
+	lwws_init
 
-   lwws_init
+	cache = "#{@cache_path}/lwws"
 
-   cache = "#{@cache_path}/lwws"
+	if date == nil
+		file_name = "#{cache}/#{convert_date( date_status)}.xml" # file_name is YYYYMMDD
+	else
+		file_name = "#{cache}/#{date}.xml"
+	end
 
-   if date == nil
-      file_name = "#{cache}/#{convert_date( date_status)}.xml" # file_name is YYYYMMDD
-   else
-      file_name = "#{cache}/#{date}.xml"
-   end
-   
-   begin
-      xml = File::read( file_name )
-      
-      doc = REXML::Document::new( xml ).root
-      telop = NKF::nkf('-We', doc.elements["telop"].text)
-      max_temp = doc.elements["temperature/max/celsius"].text
-      min_temp = doc.elements["temperature/min/celsius"].text
-      detail_url = doc.elements["link"].text
+	begin
+		xml = File::read( file_name )
 
-      result = ""
-      result << %Q|<div class=\"lwws\">|
+		doc = REXML::Document::new( xml ).root
+		telop = NKF::nkf('-We', doc.elements["telop"].text)
+		max_temp = doc.elements["temperature/max/celsius"].text
+		min_temp = doc.elements["temperature/min/celsius"].text
+		detail_url = doc.elements["link"].text
 
-      if @conf['lwws.icon.disp'] != "t" || @conf.mobile_agent? then
-         result << %Q|<a href="#{detail_url}">#{telop}</a>|
-      else
-         title = NKF::nkf('-We', doc.elements["image/title"].text)
-         link = doc.elements["image/link"].text
-         url = doc.elements["image/url"].text
-         width = doc.elements["image/width"].text
-         height = doc.elements["image/height"].text
-         result << %Q|<a href="#{link}"><img src="#{url}" border="0" alt="#{title}" title="#{title}" width=#{width} height="#{height}" /></a>|
-      end
+		result = ""
+		result << %Q|<div class=\"lwws\">|
 
-      if @conf['lwws.max_temp.disp'] == "t" then
-         unless max_temp == nil
-            result << %Q| #{@lwws_max_temp_label}:#{max_temp}#{@celsius_label}|
-         end
-      end
-      if @conf['lwws.min_temp.disp'] == "t" then
-         unless min_temp == nil
-            result << %Q| #{@lwws_min_temp_label}:#{min_temp}#{@celsius_label}|
-         end
-      end
-      
-      result << %Q|</div>|
+		if @conf['lwws.icon.disp'] != "t" || @conf.mobile_agent? then
+			result << %Q|<a href="#{detail_url}">#{telop}</a>|
+		else
+			title = NKF::nkf('-We', doc.elements["image/title"].text)
+			link = doc.elements["image/link"].text
+			url = doc.elements["image/url"].text
+			width = doc.elements["image/width"].text
+			height = doc.elements["image/height"].text
+			result << %Q|<a href="#{link}"><img src="#{url}" border="0" alt="#{title}" title="#{title}" width=#{width} height="#{height}" /></a>|
+		end
 
-      return result
-      
-   rescue Errno::ENOENT
-      return ''
-   end
+		if @conf['lwws.max_temp.disp'] == "t" then
+			unless max_temp == nil
+				result << %Q| #{@lwws_max_temp_label}:#{max_temp}#{@celsius_label}|
+			end
+		end
+		if @conf['lwws.min_temp.disp'] == "t" then
+			unless min_temp == nil
+				result << %Q| #{@lwws_min_temp_label}:#{min_temp}#{@celsius_label}|
+			end
+		end
+
+		result << %Q|</div>|
+
+		return result
+
+	rescue Errno::ENOENT
+		return ''
+	end
 end
 
 def lwws_conf_proc
-   lwws_init
-   
+	lwws_init
+
 	if @mode == 'saveconf' then
-      @conf['lwws.city_id'] = @cgi.params['lwws.city_id'][0].to_i
-      @conf['lwws.icon.disp'] = @cgi.params['lwws.icon.disp'][0]
-      @conf['lwws.max_temp.disp'] = @cgi.params['lwws.max_temp.disp'][0]
-      @conf['lwws.min_temp.disp'] = @cgi.params['lwws.min_temp.disp'][0]
-      @conf['lwws.cache'] = @cgi.params['lwws.cache'][0]
-      @conf['lwws.cache_time'] = @cgi.params['lwws.cache_time'][0].to_i
+		@conf['lwws.city_id'] = @cgi.params['lwws.city_id'][0].to_i
+		@conf['lwws.icon.disp'] = @cgi.params['lwws.icon.disp'][0]
+		@conf['lwws.max_temp.disp'] = @cgi.params['lwws.max_temp.disp'][0]
+		@conf['lwws.min_temp.disp'] = @cgi.params['lwws.min_temp.disp'][0]
+		@conf['lwws.cache'] = @cgi.params['lwws.cache'][0]
+		@conf['lwws.cache_time'] = @cgi.params['lwws.cache_time'][0].to_i
 	end
 
 	result = ''
 
-   result << <<-HTML
-   <h3>#{@lwws_label_city_id}</h3>
-   <p>#{@lwws_desc_city_id}</p>
-   <p><input name="lwws.city_id" value="#{@conf['lwws.city_id']}"></p>
-   HTML
-   
-   result << %Q|<h3>#{@lwws_icon_label}</h3>|
-   checked = "t" == @conf['lwws.icon.disp'] ? ' checked' : ''
-   result << %Q|<p><input name="lwws.icon.disp" type="checkbox" value="t"#{checked} >#{@lwws_icon_desc}</p>|
+	result << <<-HTML
+	<h3>#{@lwws_label_city_id}</h3>
+	<p>#{@lwws_desc_city_id}</p>
+	<p><input name="lwws.city_id" value="#{@conf['lwws.city_id']}"></p>
+	HTML
 
-   result << %Q|<h3>#{@lwws_label_disp_item}</h3>|
-   result << %Q|<p>#{@lwws_desc_disp_item}</p>|
-   result << %Q|<ul>|
-   checked = "t" == @conf['lwws.max_temp.disp'] ? ' checked' : ''
-   result << %Q|<li><input name="lwws.max_temp.disp" type="checkbox" value="t"#{checked} >#{@lwws_max_temp_label}</li>|
-   checked = "t" == @conf['lwws.min_temp.disp'] ? ' checked' : ''
-   result << %Q|<li><input name="lwws.min_temp.disp" type="checkbox" value="t"#{checked} >#{@lwws_min_temp_label}</li>|
-   result << %Q|</ul>|
+	result << %Q|<h3>#{@lwws_icon_label}</h3>|
+	checked = "t" == @conf['lwws.icon.disp'] ? ' checked' : ''
+	result << %Q|<p><input name="lwws.icon.disp" type="checkbox" value="t"#{checked} >#{@lwws_icon_desc}</p>|
 
-   result << %Q|<h3>#{@lwws_label_cache}</h3>|
-   checked = "t" == @conf['lwws.cache'] ? ' checked' : ''
-   result << %Q|<p><input name="lwws.cache" type="checkbox" value="t"#{checked} >#{@lwws_desc_cache}</p>|
-   result << %Q|<p>#{@lwws_desc_cache_time}</p>|
-   result << %Q|<p><input name="lwws.cache_time" value="#{@conf['lwws.cache_time']}"></p>|
-   return result
-end
+	result << %Q|<h3>#{@lwws_label_disp_item}</h3>|
+	result << %Q|<p>#{@lwws_desc_disp_item}</p>|
+	result << %Q|<ul>|
+	checked = "t" == @conf['lwws.max_temp.disp'] ? ' checked' : ''
+	result << %Q|<li><input name="lwws.max_temp.disp" type="checkbox" value="t"#{checked} >#{@lwws_max_temp_label}</li>|
+	checked = "t" == @conf['lwws.min_temp.disp'] ? ' checked' : ''
+	result << %Q|<li><input name="lwws.min_temp.disp" type="checkbox" value="t"#{checked} >#{@lwws_min_temp_label}</li>|
+	result << %Q|</ul>|
 
-add_body_enter_proc do |date|
-   lwws_to_html( "", date.strftime("%Y%m%d"))
-end
-
-add_update_proc do
-   lwws_get( "today" )
-end
-
-add_conf_proc( 'lwws', @lwws_plugin_name ) do
-   lwws_conf_proc
+	result << %Q|<h3>#{@lwws_label_cache}</h3>|
+	checked = "t" == @conf['lwws.cache'] ? ' checked' : ''
+	result << %Q|<p><input name="lwws.cache" type="checkbox" value="t"#{checked} >#{@lwws_desc_cache}</p>|
+	result << %Q|<p>#{@lwws_desc_cache_time}</p>|
+	result << %Q|<p><input name="lwws.cache_time" value="#{@conf['lwws.cache_time']}"></p>|
+	return result
 end
 
 def lwws_today
-   lwws_get( "today" )
-   lwws_to_html( "today" )
+	lwws_get( "today" )
+	lwws_to_html( "today" )
 end
 
 def lwws_tomorrow
-   lwws_get( "tomorrow" )
-   lwws_to_html( "tomorrow" )
+	lwws_get( "tomorrow" )
+	lwws_to_html( "tomorrow" )
 end
 
 def lwws_dayaftertomorrow
-   lwws_get( "dayaftertomorrow" )
-   lwws_to_html( "dayaftertomorrow" )
+	lwws_get( "dayaftertomorrow" )
+	lwws_to_html( "dayaftertomorrow" )
 end
 
 def lwws( date )
-   lwws_to_html( "", date )
+	lwws_to_html( "", date )
+end
+
+add_body_enter_proc do |date|
+	lwws_to_html( "", date.strftime("%Y%m%d"))
+end
+
+add_update_proc do
+	lwws_get( "today" )
+end
+
+add_conf_proc( 'lwws', @lwws_plugin_name ) do
+	lwws_conf_proc
 end
---
+++ contrib/plugin/livedoor/weather_index.rb
@@ -0,0 +1,157 @@
+#!/usr/bin/env ruby
+
+require 'cgi'
+require 'time'
+require 'nkf'
+require 'rexml/document'
+
+@index = "/path/to/this/cgi"
+@lwws_path = "/path/to/diary/cache/lwws"
+@diary_index = "/path/to/diary/cgi"
+@theme = "default"
+
+def calendar2_make_cal(year, month)
+	result = []
+	t = Time.local(year, month, 1)
+	r = Array.new(t.wday, nil)
+	r << 1
+	2.upto(31) do |i|
+		break if Time.local(year, month, i).month != month
+		r << i
+	end
+	r += Array.new((- r.size) % 7, nil)
+	0.step(r.size - 1, 7) do |i|
+		result << r[i, 7]
+	end
+	result
+end
+
+def calendar2_make_anchor(ym, str)
+	if ym
+		%Q|<a href="#{@index}?date=#{ym}">#{str}</a>|
+	else
+		str
+	end
+end
+
+def calendar2_prev_current_next(date)
+	yyyymm = date.strftime("%Y%m")
+	if( date.month.to_i == 1 )
+			yms = [(date.year.to_i - 1).to_s + "12", yyyymm, yyyymm.to_i + 1]
+	elsif( date.month.to_i == 12 )
+			yms = [yyyymm.to_i - 1, yyyymm, (date.year.to_i + 1).to_s + "01"]
+	else
+			yms = [yyyymm.to_i - 1, yyyymm, yyyymm.to_i + 1]
+	end
+	return yms
+end
+
+def lwws_to_html( date )
+	file_name = "#{@lwws_path}/#{date}.xml"
+
+	begin
+		xml = File::read( file_name )
+
+		doc = REXML::Document::new( xml ).root
+		title = NKF::nkf('-We', doc.elements["image/title"].text)
+		url = doc.elements["image/url"].text
+		width = doc.elements["image/width"].text
+		height = doc.elements["image/height"].text
+
+		result = ""
+		result << %Q|<div class=\"lwws\">|
+		result << %Q|<img src="#{url}" border="0" alt="#{title}" title="#{title}" width=#{width} height="#{height}" />|
+		result << %Q|</div>|
+
+		return result
+
+	rescue Errno::ENOENT
+		return ''
+	end
+end
+
+def make_weather_cal( date )
+
+	ymd = Time.parse(date + "01")
+
+	year = ymd.year
+	month = ymd.month
+	p_c_n = calendar2_prev_current_next( ymd )
+	days_format = ["日","月","火","水","木","金","土"]
+	navi_format = ["前", "%d年<br>%d月", "次"]
+
+	r = ""
+	r << %Q|<div class="day">|
+	r << %Q|<div class="body"><table><tr><td colspan="7"></td></tr>|
+	r << %Q|<tr>|
+	r << %Q|<td align="center" colspan="2">#{calendar2_make_anchor(p_c_n[0], navi_format[0] % [year, month])}</td>|
+	r << %Q|<td align="center" colspan="3">#{calendar2_make_anchor(p_c_n[1], navi_format[1] % [year, month])}</td>|
+	r << %Q|<td align="center" colspan="2">#{calendar2_make_anchor(p_c_n[2], navi_format[2] % [year, month])}</td>|
+	r << %Q|</tr>|
+	r << "<tr>"
+
+	0.upto(6) do |i|
+		r <<%Q|<td align="center">#{days_format[i]}</td>|
+	end
+
+	r << "</tr>\n"
+
+	calendar2_make_cal(year, month).each do |week|
+		r << %Q|<tr>|
+		week.each do |day|
+			if day == nil
+				r <<  %Q| <td></td>\n|
+			else
+				date = "%04d%02d%02d" % [year, month, day]
+				r << %Q| <td align="center">#{day.to_s}日<br />%s</td>\n| % lwws_to_html(date)
+			end
+		end
+		r << %Q|</tr>|
+	end
+
+	r << %Q|</table>|
+	r << %Q|</div>|
+	r << %Q|</div>|
+
+	return r
+end
+
+BEGIN { $defout.binmode }
+$KCODE = 'n'
+
+begin
+	@cgi = CGI::new
+	date = @cgi.params['date'][0]
+	date = Time.now.strftime("%Y%m") if not date or not /^\d{6}$/ =~ date
+
+	print @cgi.header('type' => 'text/html', 'charset' => 'euc-jp')
+	print %Q[
+		<html>
+		<head>
+		<title>livedoor Weather Calendar</title>
+		<link rel="stylesheet" href="#{@diary_index}/theme/base.css" type="text/css" media="all">
+		<link rel="stylesheet" href="#{@diary_index}/theme/#{@theme}/#{@theme}.css" title="pukiwiki" type="text/css" media="all">
+		</head>
+		<body>
+		<h1>livedoor Weather Calendar</h1>
+	]
+	print make_weather_cal( date )
+	print %Q[
+		</body>
+		</html>
+	]
+rescue
+	if @cgi then
+		print @cgi.header( 'status' => '500 Internal Server Error', 'type' => 'text/html' )
+	else
+		print "Status: 500 Internal Server Error\n"
+		print "Content-Type: text/html\n\n"
+	end
+	puts "<h1>500 Internal Server Error</h1>"
+	puts "<pre>"
+	puts "#$! (#{$!.class})"
+	puts ""
+	puts $@.join( "\n" )
+	puts "</pre>"
+	puts "<div>#{' ' * 500}</div>"
+end
