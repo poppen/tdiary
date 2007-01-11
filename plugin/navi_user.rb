6476e5422fb32a13599e4a7c8d1f72c63a84acde
zunda <zunda@7f22e88f-374d-0410-998f-c91420d97ba2>
2007-01-11 14:32:08 +0000

    navi_user.rb: modified to check @date before going further. @date is nil in configuration mode.

--- plugin/navi_user.rb
+++ plugin/navi_user.rb
@@ -1,4 +1,4 @@
-# navi_user.rb $Revision: 1.10 $
+# navi_user.rb $Revision: 1.11 $
 #
 # navi_user: 前日，翌日→前の日記，次の日記
 #   modeがday/commentのときに表示される「前日」「翌日」ナビゲーション
@@ -17,37 +17,41 @@ end
 MODIFY_CLASS
 
 add_header_proc do
-	cgi = CGI.new
-	def cgi.referer; nil; end
-	days = []
-	yms = []
-	today = @date.strftime('%Y%m%d')
-	this_month = @date.strftime('%Y%m')
+	if @date then
+		cgi = CGI.new
+		def cgi.referer; nil; end
+		days = []
+		yms = []
+		today = @date.strftime('%Y%m%d')
+		this_month = @date.strftime('%Y%m')
 
-	@years.keys.each do |y|
-		yms += @years[y].collect {|m| y + m}
+		@years.keys.each do |y|
+			yms += @years[y].collect {|m| y + m}
+		end
+		yms |= [this_month]
+		yms.sort!
+		yms.unshift(nil).push(nil)
+		yms[yms.index(this_month) - 1, 3].each do |ym|
+			next unless ym
+			cgi.params['date'] = [ym]
+			m = TDiaryMonth.new(cgi, '', @conf)
+			days += m.diaries.keys.sort
+		end
+		days |= [today]
+		days.sort!
+		days.unshift(nil).push(nil)
+		@navi_user_days = days[days.index(today) - 1, 3]
 	end
-	yms |= [this_month]
-	yms.sort!
-	yms.unshift(nil).push(nil)
-	yms[yms.index(this_month) - 1, 3].each do |ym|
-		next unless ym
-		cgi.params['date'] = [ym]
-		m = TDiaryMonth.new(cgi, '', @conf)
-		days += m.diaries.keys.sort
-	end
-	days |= [today]
-	days.sort!
-	days.unshift(nil).push(nil)
-	@navi_user_days = days[days.index(today) - 1, 3]
 	''
 end
 
 def navi_user_day
 	result = ''
-	result << navi_item( "#{h @index}#{anchor @navi_user_days[0]}", "&laquo;#{h navi_prev_diary(navi_user_format(@navi_user_days[0]))}" ) if @navi_user_days[0]
-	result << navi_item( h(@index), h(navi_latest) )
+	if @navi_user_days then
+		result << navi_item( "#{h @index}#{anchor @navi_user_days[0]}", "&laquo;#{h navi_prev_diary(navi_user_format(@navi_user_days[0]))}" ) if @navi_user_days[0]
+		result << navi_item( h(@index), h(navi_latest) )
 	result << navi_item( "#{h @index}#{anchor @navi_user_days[2]}", "#{h navi_next_diary(navi_user_format(@navi_user_days[2]))}&raquo;" ) if @navi_user_days[2]
+	end
 	result
 end
 
