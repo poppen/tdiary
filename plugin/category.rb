964e606af48a492019360364aa3cf2800b6aa878
zunda <zunda@7f22e88f-374d-0410-998f-c91420d97ba2>
2007-01-11 06:22:14 +0000

    category.rb: modified to escape category names in sub titles (as is done in the other place) and URL-escape category names in URLs. We might still have to take care of charactor codes before URL-escaping category names.

--- plugin/category.rb
+++ plugin/category.rb
@@ -1,4 +1,4 @@
-# category.rb $Revision: 1.34 $
+# category.rb $Revision: 1.35 $
 #
 # Copyright (c) 2003 Junichiro KITA <kita@kitaj.no-ip.com>
 # Distributed under the GPL
@@ -56,9 +56,9 @@ def category_anchor(category)
 			""
 		end
 	if @category_icon[category]
-		%Q|<a href="#{h @index}?#{period_string}category=#{h category}"><img class="category" src="#{h @category_icon_url}#{h @category_icon[category]}" alt="#{category}"></a>|
+		%Q|<a href="#{h @index}?#{period_string}category=#{h(u(category))}"><img class="category" src="#{h @category_icon_url}#{h @category_icon[category]}" alt="#{h category}"></a>|
 	else
-		%Q|[<a href="#{h @index}?#{period_string}category=#{h category}">#{category}</a>]|
+		%Q|[<a href="#{h @index}?#{period_string}category=#{h(u(category))}">#{h category}</a>]|
 	end
 end
 
@@ -267,7 +267,7 @@ public
 	end
 
 	def make_anchor(label = nil)
-		a = @category.map {|c| "category=#{h c}"}.join(';')
+		a = @category.map {|c| "category=#{h(u(c))}"}.join(';')
 		a << ";year=#{@year}" if @year
 		a << ";month=#{@month}" if @month
 		if label
