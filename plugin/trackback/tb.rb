ee739e45730c8598f6f0c568612023531db5eadc
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2007-01-17 09:34:42 +0000

    * trackback/tb.rb: support TrackBack spec 1.2.

--- plugin/trackback/tb.rb
+++ plugin/trackback/tb.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# tb.rb $Revision: 1.10 $
+# tb.rb $Revision: 1.11 $
 #
 # Copyright (c) 2003 Junichiro KITA <kita@kitaj.no-ip.com>
 # Distributed under the GPL
@@ -20,47 +20,32 @@ begin
 	require 'tdiary'
   
 	@cgi = CGI::new
-	conf = TDiary::Config::new(@cgi)
+	conf = TDiary::Config::new( @cgi )
 	tdiary = nil
 
 	begin
 		if /POST/i =~ @cgi.request_method and @cgi.valid?( 'url' ) and
-				! @cgi.referer and
-				/^Mozilla\// !~ @cgi.user_agent then
+			   ! @cgi.referer and /^Mozilla\// !~ @cgi.user_agent then
 			tdiary = TDiary::TDiaryTrackBackReceive::new( @cgi, 'day.rhtml', conf )
-		elsif @cgi.valid?( '__mode') and @cgi.params['__mode'][0] == 'rss'
-			tdiary = TDiary::TDiaryTrackBackRSS::new( @cgi, nil, conf )
 		end
 	rescue TDiary::TDiaryError
 	end
-	tdiary = TDiary::TDiaryTrackBackShow::new( @cgi, nil, conf ) unless tdiary
 
-	head = {
-		#'type' => 'application/xml',
-		'type' => 'text/xml',
-		'Vary' => 'User-Agent'
-	}
-	body = ''
+	tdiary = TDiary::TDiaryTrackBackShow::new( @cgi, nil, conf ) unless tdiary
 	begin
 		body = tdiary.eval_rhtml
-		head['charset'] = conf.encoding
-		head['Content-Length'] = body.size.to_s
-		head['Pragma'] = 'no-cache'
-		head['Cache-Control'] = 'no-cache'
+		head = {
+			'type' => 'text/xml',
+			'charset' => conf.encoding,
+			'Content-Length' => body.size.to_s
+		}
 		print @cgi.header( head )
 		print body
 	rescue TDiary::TDiaryTrackBackError
-		head = {
-			#'type' => 'application/xml',
-			'type' => 'text/xml'
-		}
-		print @cgi.header( head )
-		print TDiary::TDiaryTrackBackBase::fail_response($!.message)
+		print @cgi.header( { 'type' => 'text/xml' } )
+		print TDiary::TDiaryTrackBackBase::fail_response( $!.message )
 	rescue TDiary::ForceRedirect
-		head = {
-			#'Location' => $!.path
-			'type' => 'text/html',
-		}
+		head = { 'type' => 'text/html' }
 		head['cookie'] = tdiary.cookies if tdiary.cookies.size > 0
 		print @cgi.header( head )
 		print %Q[
