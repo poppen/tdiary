b00578f3ce2cf8bd6ac1874c8e53c1500980937c
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2007-02-19 14:08:32 +0000

    * add new plugin: edit_today.rb.

---
+++ plugin/edit_today.rb
@@ -0,0 +1,43 @@
+# edit_today.rb : add link to edit after title of each days. $Revision: 1.1 $
+#
+# Copyright (C) 2007 by NOB <nob@harunaru.com>
+# You can redistribute it and/or modify it under GPL2.
+#
+
+def edit_today_init
+	@conf['edit_today.caption'] ||= @edit_today_caption
+end
+
+add_title_proc do |date,title|
+	edit_today_link ( date, title )
+end
+
+def edit_today_link ( date, title )
+	unless @mode == 'day'
+		edit_today_init
+		caption = @conf['edit_today.caption']
+		unless @conf.mobile_agent?
+			r = <<-HTML
+			#{title}\n<span class="edit-today">
+			<a href="#{@update}?edit=true;#{date.strftime( 'year=%Y;month=%m;day=%d' )}" title="#{edit_today_edit_label( date )}">#{caption}</a>
+			</span>
+			HTML
+		else
+			title
+		end
+	else
+		title
+	end
+end
+
+def edit_today_saveconf
+	if @mode == 'saveconf' then
+		@conf['edit_today.caption'] = @cgi.params['edit_today_caption'][0]
+	end
+end
+
+add_conf_proc( 'edit_today', @edit_today_caption ) do
+	edit_today_saveconf
+	edit_today_init
+	edit_today_conf_html
+end
---
+++ plugin/zh/edit_today.rb
@@ -0,0 +1,15 @@
+# English resources of edit_today plugin.
+
+@edit_today_caption = 'Edit'
+
+def edit_today_edit_label( date )
+	date.strftime( 'Edit %Y-%m-%d' )
+end
+
+def edit_today_conf_html
+	r = <<-HTML
+	<h3 class="subtitle">Link String</h3>
+	<p>Specify string to link to edit page. If you have image file, you can specify icon on this link.</p>
+	<p><input name="edit_today_caption" size="70" value="#{@conf['edit_today.caption']}"></p>
+	HTML
+end
