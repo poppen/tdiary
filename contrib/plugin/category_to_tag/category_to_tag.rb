c9f572170f0f94f3c6702553a7a0bb3a0ff69191
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2007-04-29 02:04:51 +0000

    * categody_to_tag: support mobile mode.

--- contrib/plugin/category_to_tag/category_to_tag.rb
+++ contrib/plugin/category_to_tag/category_to_tag.rb
@@ -1,5 +1,5 @@
 # category_to_tag.rb - show categories list in end of each section
-# $Revision: 1.5 $
+# $Revision: 1.6 $
 #
 # Copyright (C) 2005, TADA Tadashi <sho@spc.gr.jp>
 # You can redistribute it and/or modify it under GPL2.
@@ -44,8 +44,6 @@ elsif respond_to?( :category_anchor ) # diary
 end
 
 def category_to_tag_list
-	return '' if @conf.mobile_agent?
-
 	if @category_to_tag_list and not @category_to_tag_list.empty? then
 		r = '<div class="tags">Tags: '
 		@category_to_tag_list.each do |tag, blog|
