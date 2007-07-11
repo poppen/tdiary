726240b2d4ad7ca6e449ce69cae099d1fac9970a
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2007-07-11 15:20:23 +0000

    * hatena_star.rb: support token but not support preferences page.

--- contrib/plugin/hatena/hatena_star.rb
+++ contrib/plugin/hatena/hatena_star.rb
@@ -1,3 +1,11 @@
+#
+# hatena_star.rb
+#
 add_header_proc do
-	%Q|\t<script type="text/javascript" src="http://s.hatena.com/js/HatenaStar.js"></script>\n|
+	<<-SCRIPT
+	<script type="text/javascript" src="http://s.hatena.ne.jp/js/HatenaStar.js"></script>
+	<script type="text/javascript"><!--
+		Hatena.Star.Token = 'ここにあなたのトークンを入力';
+	--></script>
+	SCRIPTend
 end
