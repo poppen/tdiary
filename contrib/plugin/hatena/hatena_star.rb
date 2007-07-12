120682b9dbe322d01217d7fe74873e4827ce13bd
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2007-07-12 14:28:55 +0000

    * hatena_star.rb: replace code by Itoshi Nikaido.

--- contrib/plugin/hatena/hatena_star.rb
+++ contrib/plugin/hatena/hatena_star.rb
@@ -1,11 +1,19 @@
-#
 # hatena_star.rb
-#
+# Itoshi Nikaido <dritoshi at gmail dot com>
+# Distributed under the GPL
 add_header_proc do
-	<<-SCRIPT
-	<script type="text/javascript" src="http://s.hatena.ne.jp/js/HatenaStar.js"></script>
-	<script type="text/javascript"><!--
-		Hatena.Star.Token = 'ここにあなたのトークンを入力';
-	--></script>
-	SCRIPTend
+	hatena_star = %Q|\t<script type="text/javascript" src="http://s.hatena.ne.jp/js/HatenaStar.js"></script>\n|
+	hatena_star << %Q|\t<script type="text/javascript"><!--\n|
+	hatena_star << %Q|\t\tHatena.Star.Token = '#{h @conf['hatena_star.token']}';\n|
+	hatena_star << %Q|\t//--></script>\n|
+end
+
+add_conf_proc( 'Hatena::Star', 'Hatena::Star') do
+	if( @mode == 'saveconf' ) then
+		@conf['hatena_star.token'] = @cgi.params['hatena_star.token'][0]
+	end
+	<<-HTML
+	<h3>Hatena::Star Token</h3>
+	<p><input name="hatena_star.token" value="#{h @conf['hatena_star.token']}" size=42></P>
+	HTML
 end
