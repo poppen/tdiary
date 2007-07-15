aa67d8c15939eb859059f27d7f6725a1d4b3acc9
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2007-07-15 07:10:19 +0000

    * hatena_star.rb: add options: some replacement images.

--- contrib/plugin/hatena/hatena_star.rb
+++ contrib/plugin/hatena/hatena_star.rb
@@ -1,19 +1,43 @@
 # hatena_star.rb
 # Itoshi Nikaido <dritoshi at gmail dot com>
 # Distributed under the GPL
+
+@hatena_star_options = {
+	'token' => 'Token',
+	'star.image' => 'Star.ImgSrc',
+	'star.add' => 'AddButton.ImgSrc',
+	'comment.image' => 'CommentButton.ImgSrc',
+	'comment.active' => 'CommentButton.ImgSrcActive'
+}
+
 add_header_proc do
 	hatena_star = %Q|\t<script type="text/javascript" src="http://s.hatena.ne.jp/js/HatenaStar.js"></script>\n|
 	hatena_star << %Q|\t<script type="text/javascript"><!--\n|
-	hatena_star << %Q|\t\tHatena.Star.Token = '#{h @conf['hatena_star.token']}';\n|
+		@hatena_star_options.each do |o,v|
+			hatena_star << %Q|\t\tHatena.Star.#{v} = '#{h @conf["hatena_star.#{o}"]}';\n| if @conf["hatena_star.#{o}"]
+		end
 	hatena_star << %Q|\t//--></script>\n|
 end
 
-add_conf_proc( 'Hatena::Star', 'Hatena::Star') do
+add_conf_proc( 'hatena_star', 'Hatena::Star' ) do
 	if( @mode == 'saveconf' ) then
-		@conf['hatena_star.token'] = @cgi.params['hatena_star.token'][0]
+		@hatena_star_options.keys.each do |o|
+			@conf["hatena_star.#{o}"] = @cgi.params["hatena_star.#{o}"][0].strip
+			if @conf["hatena_star.#{o}"].length == 0 then
+				@conf["hatena_star.#{o}"] = nil
+			end
+		end
 	end
 	<<-HTML
-	<h3>Hatena::Star Token</h3>
-	<p><input name="hatena_star.token" value="#{h @conf['hatena_star.token']}" size=42></P>
+	<h3>Token</h3>
+	<p><input name="hatena_star.token" value="#{h @conf['hatena_star.token']}" size=50></P>
+	<h3>Star Image (URL)</h3>
+	<p><input name="hatena_star.star.image" value="#{h @conf['hatena_star.star.image']}" size=50></P>
+	<h3>Add Star Image (URL)</h3>
+	<p><input name="hatena_star.star.add" value="#{h @conf['hatena_star.star.add']}" size=50></P>
+	<h3>Comment Image (URL)</h3>
+	<p><input name="hatena_star.comment.image" value="#{h @conf['hatena_star.comment.image']}" size=50></P>
+	<h3>Active Comment Image (URL)</h3>
+	<p><input name="hatena_star.comment.active" value="#{h @conf['hatena_star.comment.active']}" size=50></P>
 	HTML
 end
