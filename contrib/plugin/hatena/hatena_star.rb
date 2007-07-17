d4f7f00c03e10aa71ab991c776252c5b7deb1b1a
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2007-07-17 06:16:36 +0000

    * hatena_star.rb: support tDiary 2.1.4.

--- contrib/plugin/hatena/hatena_star.rb
+++ contrib/plugin/hatena/hatena_star.rb
@@ -14,7 +14,7 @@ add_header_proc do
 	hatena_star = %Q|\t<script type="text/javascript" src="http://s.hatena.ne.jp/js/HatenaStar.js"></script>\n|
 	hatena_star << %Q|\t<script type="text/javascript"><!--\n|
 		@hatena_star_options.each do |o,v|
-			hatena_star << %Q|\t\tHatena.Star.#{v} = '#{h @conf["hatena_star.#{o}"]}';\n| if @conf["hatena_star.#{o}"]
+			hatena_star << %Q|\t\tHatena.Star.#{v} = '#{CGI::escapeHTML @conf["hatena_star.#{o}"]}';\n| if @conf["hatena_star.#{o}"]
 		end
 	hatena_star << %Q|\t//--></script>\n|
 end
@@ -30,14 +30,14 @@ add_conf_proc( 'hatena_star', 'Hatena::Star' ) do
 	end
 	<<-HTML
 	<h3>Token</h3>
-	<p><input name="hatena_star.token" value="#{h @conf['hatena_star.token']}" size=50></P>
+	<p><input name="hatena_star.token" value="#{CGI::escapeHTML @conf['hatena_star.token']}" size=50></P>
 	<h3>Star Image (URL)</h3>
-	<p><input name="hatena_star.star.image" value="#{h @conf['hatena_star.star.image']}" size=50></P>
+	<p><input name="hatena_star.star.image" value="#{CGI::escapeHTML @conf['hatena_star.star.image']}" size=50></P>
 	<h3>Add Star Image (URL)</h3>
-	<p><input name="hatena_star.star.add" value="#{h @conf['hatena_star.star.add']}" size=50></P>
+	<p><input name="hatena_star.star.add" value="#{CGI::escapeHTML @conf['hatena_star.star.add']}" size=50></P>
 	<h3>Comment Image (URL)</h3>
-	<p><input name="hatena_star.comment.image" value="#{h @conf['hatena_star.comment.image']}" size=50></P>
+	<p><input name="hatena_star.comment.image" value="#{CGI::escapeHTML @conf['hatena_star.comment.image']}" size=50></P>
 	<h3>Active Comment Image (URL)</h3>
-	<p><input name="hatena_star.comment.active" value="#{h @conf['hatena_star.comment.active']}" size=50></P>
+	<p><input name="hatena_star.comment.active" value="#{CGI::escapeHTML @conf['hatena_star.comment.active']}" size=50></P>
 	HTML
 end
