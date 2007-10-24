7ce204279970eaf86257afb187e14c8d2597b535
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2007-10-24 11:59:52 +0000

    * plugin/youtube: make link to video in mobile mode.

--- contrib/plugin/youtube/youtube.rb
+++ contrib/plugin/youtube/youtube.rb
@@ -6,7 +6,11 @@
 # usage: <%= youtube 'VIDEO_ID' %>
 #
 def youtube( video_id )
-	<<-TAG
-	<object width="425" height="350"><param name="movie" value="http://www.youtube.com/v/#{video_id}"></param><embed src="http://www.youtube.com/v/#{video_id}" type="application/x-shockwave-flash" width="425" height="350"></embed></object>
-	TAG
+	if @conf.mobile_agent?
+		%Q|<div class="youtube"><a href="http://www.youtube.com/watch?v=#{video_id}">YouTube (#{video_id})</a></div>|
+	else
+		<<-TAG
+		<object width="425" height="350"><param name="movie" value="http://www.youtube.com/v/#{video_id}"></param><embed src="http://www.youtube.com/v/#{video_id}" type="application/x-shockwave-flash" width="425" height="350"></embed></object>
+		TAG
+	end
 end
