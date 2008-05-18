55c58b182c06a6645b679ae44fffc8ddfa25728b
hsbt <hsbt@7f22e88f-374d-0410-998f-c91420d97ba2>
2008-05-18 21:27:45 +0000

    * replace contrib directory for CodeRepos.

--- contrib/util/estraier-search/en/estraier-register.rb
+++
@@ -1,3 +0,0 @@
-@estraier_register_conf_label = 'Estraier Search'
-@estraier_register_conf_header = 'Rebuild Estraier search index'
-@estraier_register_conf_description = 'To rebuild Estraier search index, check the box and submit \'OK\'.'
--- contrib/util/estraier-search/ja/estraier-register.rb
+++
@@ -1,3 +0,0 @@
-@estraier_register_conf_label = 'Estraier検索'
-@estraier_register_conf_header = 'Estraier検索インデックスの再構築'
-@estraier_register_conf_description = 'Estraier検索のインデックスを再構築する場合は、チェックボックスをチェックしてOKを押してください。'
