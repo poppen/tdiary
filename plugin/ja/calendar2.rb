7bbfcc6bd18d8435ac58f7b9a35f7ee50c32dc97
hsbt <hsbt@7f22e88f-374d-0410-998f-c91420d97ba2>
2009-04-27 10:38:36 +0000

    modified language resource, changed Array of String.

--- plugin/ja/calendar2.rb
+++ plugin/ja/calendar2.rb
@@ -1,5 +1,5 @@
+# -*- coding: utf-8 -*-
 # calendar2.rb language recource for Japanese
 
-@calendar2_days_format = "日月火水木金土".split(//)
+@calendar2_days_format = ["日", "月", "火", "水", "木", "金", "土"]
 @calendar2_navi_format = ["前", "%d年<br>%d月", "次"]
-
